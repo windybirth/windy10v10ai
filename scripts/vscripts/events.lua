39ee5cab065ccc961e3e390c7d3e15a927e76bef
windy <windybirth@gmail.com>
2021-08-15 15:12:04 +0900

    increase roshan

--- scripts/vscripts/events.lua
+++ scripts/vscripts/events.lua
@@ -361,7 +361,7 @@ end
 function AIGameMode:OnPlayerLevelUp(keys)
 	local iEntIndex=PlayerResource:GetPlayer(keys.player-1):GetAssignedHero():entindex()
 	Timers:CreateTimer(0.5, function ()
-		EntIndexToHScript(iEntIndex):SetCustomDeathXP(40 + EntIndexToHScript(iEntIndex):GetCurrentXP()*0.14)
+		EntIndexToHScript(iEntIndex):SetCustomDeathXP(40 + EntIndexToHScript(iEntIndex):GetCurrentXP()*0.1)
 	end)
 
 
