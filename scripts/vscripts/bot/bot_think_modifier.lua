93262740aada0da260b32ee4f4706c4c34727055
windy <windybirth@gmail.com>
2021-12-29 20:04:54 +0900

    代码优化 #58 AI技能释放 #124

--- scripts/vscripts/bot/bot_think_modifier.lua
+++ scripts/vscripts/bot/bot_think_modifier.lua
@@ -1,5 +1,6 @@
 
 require('bot/bot_think_item_use')
+require('bot/bot_think_ability_use')
 
 --------------------------------------------------------------------------------
 -- Bot item use modifier
@@ -17,5 +18,72 @@ end
 
 function modifier_bot_think_item_use:OnIntervalThink()
 	if IsClient() then return end
-	UseActiveItem(self:GetParent())
+
+	local hHero = self:GetParent()
+	-- if hero is dead, do nothing
+	if hHero:IsAlive() == false then return end
+
+	-- item use
+	if hHero:IsStunned() then return end
+	UseActiveItem(hHero)
+
+	-- ability use
+	if hHero:IsSilenced() then return end
+	BotAbilityThink:ThinkUseAbility(hHero)
+end
+
+--------------------------------------------------------------------------------
+-- Bot strategy modifier 机器人策略
+--------------------------------------------------------------------------------
+modifier_bot_think_strategy = class({})
+
+function modifier_bot_think_strategy:IsPurgable() return false end
+function modifier_bot_think_strategy:IsHidden() return true end
+function modifier_bot_think_strategy:RemoveOnDeath() return false end
+
+function modifier_bot_think_strategy:OnCreated()
+	if IsClient() then return end
+	self:StartIntervalThink(2)
+end
+
+function modifier_bot_think_strategy:OnIntervalThink()
+	if IsClient() then return end
+
+	local GameTime = GameRules:GetDOTATime(false, false)
+	if (GameTime >= (50 * 60)) then						-- LATEGAME
+		GameRules:GetGameModeEntity():SetBotsInLateGame(true)
+		GameRules:GetGameModeEntity():SetBotsAlwaysPushWithHuman(false)
+		GameRules:GetGameModeEntity():SetBotsMaxPushTier(-1)
+	elseif (GameTime >= (18 * 60)) then					-- LATEGAME
+		if AIGameMode.barrackPushedGood > 5 or AIGameMode.barrackPushedBad > 5 then
+			GameRules:GetGameModeEntity():SetBotsMaxPushTier(-1)
+		else
+			GameRules:GetGameModeEntity():SetBotsMaxPushTier(5)
+		end
+	elseif (GameTime >= (16 * 60)) then						-- MIDGAME
+		GameRules:GetGameModeEntity():SetBotsInLateGame(true)
+		GameRules:GetGameModeEntity():SetBotsAlwaysPushWithHuman(false)
+		GameRules:GetGameModeEntity():SetBotsMaxPushTier(4)
+	elseif (GameTime >= (14 * 60)) then						-- MIDGAME
+		GameRules:GetGameModeEntity():SetBotsInLateGame(true)
+		GameRules:GetGameModeEntity():SetBotsAlwaysPushWithHuman(true)
+		GameRules:GetGameModeEntity():SetBotsMaxPushTier(3)
+	elseif (GameTime >= (10 * 60)) then						-- MIDGAME
+		GameRules:GetGameModeEntity():SetBotsInLateGame(true)
+		GameRules:GetGameModeEntity():SetBotsAlwaysPushWithHuman(true)
+		GameRules:GetGameModeEntity():SetBotsMaxPushTier(2)
+	else													-- EARLYGAME
+		GameRules:GetGameModeEntity():SetBotsInLateGame(false)
+		GameRules:GetGameModeEntity():SetBotsAlwaysPushWithHuman(true)
+		GameRules:GetGameModeEntity():SetBotsMaxPushTier(1)
+	end
+
+	local hParent = self:GetParent()
+
+	BotThink:ThinkSell(hParent)
+	BotThink:ThinkPurchase(hParent)
+	BotThink:ThinkPurchaseNeutral(hParent, GameTime)
+
+	BotThink:ThinkConsumeItem(hParent)
+	BotThink:AddMoney(hParent)
 end
