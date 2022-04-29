b9ae667bcb2453bc03a3e5ffc2b4a39f0796ed85
windy <windybirth@gmail.com>
2022-04-30 00:40:47 +0900

    霍乱A杖卡顿 #254 大A帐buff失效 #238

--- scripts/vscripts/items/item_ultimate_scepter_2.lua
+++ scripts/vscripts/items/item_ultimate_scepter_2.lua
@@ -1,7 +1,11 @@
 function Scepter2OnCreated(keys)
-	--if not keys.caster:HasModifier("modifier_item_ultimate_scepter") then
-		keys.caster:AddNewModifier(keys.caster, nil, "modifier_item_ultimate_scepter", {duration = -1})
-	--end
+	-- bane A杖大招会卡顿，移除A杖效果
+	if keys.caster:GetUnitName() ~= "npc_dota_hero_bane" then
+		-- add modifier_item_ultimate_scepter after 0.1s
+		Timers:CreateTimer(0.1, function()
+			keys.caster:AddNewModifier(keys.caster, nil, "modifier_item_ultimate_scepter", {duration = -1})
+		end)
+	end
 end
 
 --[[ ============================================================================================================
@@ -11,30 +15,33 @@ end
 	Aghanim's Scepter exist in the player's inventory.
 ================================================================================================================= ]]
 function Scepter2OnDestroy(keys)
-	local num_scepters_in_inventory = 0
-	for i=0, 5, 1 do --Search for Aghanim's Regalia in the player's inventory.
-		local current_item = keys.caster:GetItemInSlot(i)
-		if current_item ~= nil then
-			local item_name = current_item:GetName()
+	Timers:CreateTimer(0.1, function()
+		local num_scepters_in_inventory = 0
+		for i=0, 5, 1 do --Search for Aghanim's Regalia in the player's inventory.
+			local current_item = keys.caster:GetItemInSlot(i)
+			if current_item ~= nil then
+				local item_name = current_item:GetName()
 
-			if item_name == "item_ultimate_scepter2" or item_name == "item_ultimate_scepter" then
-				num_scepters_in_inventory = num_scepters_in_inventory + 1
+				if item_name == "item_ultimate_scepter_2" or item_name == "item_ultimate_scepter" then
+					num_scepters_in_inventory = num_scepters_in_inventory + 1
+				end
 			end
 		end
-	end
-	--Remove the stock Aghanim's Scepter modifier if the player no longer has a Scepter in their inventory.
-	if num_scepters_in_inventory == 0 and keys.caster:HasModifier("modifier_item_ultimate_scepter") and not keys.caster:HasModifier("modifier_item_ultimate_scepter_2_consumed") then
-		keys.caster:RemoveModifierByName("modifier_item_ultimate_scepter")
-	end
+		--Remove the stock Aghanim's Scepter modifier if the player no longer has a Scepter in their inventory.
+		if num_scepters_in_inventory == 0 and keys.caster:HasModifier("modifier_item_ultimate_scepter") and not keys.caster:HasModifier("modifier_item_ultimate_scepter_2_consumed") then
+			keys.caster:RemoveModifierByName("modifier_item_ultimate_scepter")
+		end
+	end)
 end
 
 --[[	Author: Hewdraw
 		Date: 17.05.2015	]]
 function Scepter2OnSpell(keys)
 	if keys.caster:IsRealHero() and keys.target:IsRealHero() and not keys.caster:HasModifier("modifier_arc_warden_tempest_double") and not keys.target:HasModifier("modifier_arc_warden_tempest_double") and not keys.target:HasModifier(keys.modifier) then
-		keys.target:AddNewModifier(keys.caster, nil, "modifier_item_ultimate_scepter", {duration = -1})
+		if keys.target:GetUnitName() ~= "npc_dota_hero_bane" then
+			keys.target:AddNewModifier(keys.caster, nil, "modifier_item_ultimate_scepter", {duration = -1})
+		end
 		keys.target:AddNewModifier(keys.caster, keys.ability, keys.modifier, {})
-		-- keys.ability:ApplyDataDrivenModifier(keys.caster, keys.target, keys.modifier, {})
 
 		keys.target:EmitSound("Hero_Alchemist.Scepter.Cast")
 		keys.caster:RemoveItem(keys.ability)
