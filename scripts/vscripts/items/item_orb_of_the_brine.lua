7103bb53a2f26b21455b4e8fef5e76c4abdf77a8
windy <windybirth@gmail.com>
2022-05-04 19:40:53 +0900

    重做苍洋魔珠配方 合成更平衡 #262 新道具 属性书 #260

--- scripts/vscripts/items/item_orb_of_the_brine.lua
+++ scripts/vscripts/items/item_orb_of_the_brine.lua
@@ -47,8 +47,10 @@ end
 --------------------------------------------------------------------------------
 
 function modifier_item_orb_of_the_brine:OnCreated( kv )
-	self.bonus_intelligence = self:GetAbility():GetSpecialValueFor( "bonus_intelligence" )
-	self.bonus_mana_regen = self:GetAbility():GetSpecialValueFor( "bonus_mana_regen" )
+	self.bonus_all_stats = self:GetAbility():GetSpecialValueFor( "bonus_all_stats" )
+	self.bonus_health = self:GetAbility():GetSpecialValueFor( "bonus_health" )
+	self.bonus_mana = self:GetAbility():GetSpecialValueFor( "bonus_mana" )
+	self.heal_increase = self:GetAbility():GetSpecialValueFor( "heal_increase" )
 end
 
 --------------------------------------------------------------------------------
@@ -56,8 +58,12 @@ end
 function modifier_item_orb_of_the_brine:DeclareFunctions()
 	local funcs =
 	{
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
 		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
-		MODIFIER_PROPERTY_MANA_REGEN_CONSTANT,
+		MODIFIER_PROPERTY_HEALTH_BONUS,
+		MODIFIER_PROPERTY_MANA_BONUS,
+		MODIFIER_PROPERTY_HEAL_AMPLIFY_PERCENTAGE_SOURCE,
 	}
 	return funcs
 end
@@ -65,11 +71,27 @@ end
 --------------------------------------------------------------------------------
 
 function modifier_item_orb_of_the_brine:GetModifierBonusStats_Intellect( params )
-	return self.bonus_intelligence
+	return self.bonus_all_stats
 end
 
-function modifier_item_orb_of_the_brine:GetModifierConstantManaRegen( params )
-	return self.bonus_mana_regen
+function modifier_item_orb_of_the_brine:GetModifierBonusStats_Agility( params )
+	return self.bonus_all_stats
+end
+
+function modifier_item_orb_of_the_brine:GetModifierBonusStats_Strength( params )
+	return self.bonus_all_stats
+end
+
+function modifier_item_orb_of_the_brine:GetModifierHealthBonus( params )
+	return self.bonus_health
+end
+
+function modifier_item_orb_of_the_brine:GetModifierManaBonus( params )
+	return self.bonus_mana
+end
+
+function modifier_item_orb_of_the_brine:GetModifierHealAmplify_PercentageSource( params )
+	return self.heal_increase
 end
 
 --------------------------------------------------------------------------------
---
+++ scripts/vscripts/items/item_tome_of_luoshu.lua
@@ -0,0 +1,75 @@
+item_tome_of_luoshu = class({})
+LinkLuaModifier("modifier_luoshu_tome", "items/item_tome_of_luoshu.lua", LUA_MODIFIER_MOTION_NONE)
+
+if IsServer() then
+	function item_tome_of_luoshu:OnSpellStart()
+        local caster = self:GetCaster()
+		if caster:HasModifier("modifier_luoshu_tome") then
+			return
+		else
+			caster:AddNewModifier(caster, self, "modifier_luoshu_tome", {})
+			EmitSoundOnClient("Item.TomeOfKnowledge", caster)
+		end
+		self:SpendCharge()
+	end
+end
+
+
+if modifier_luoshu_tome == nil then modifier_luoshu_tome = class({}) end
+
+function modifier_luoshu_tome:RemoveOnDeath() return false end
+function modifier_luoshu_tome:IsPermanent() return true end
+
+function modifier_luoshu_tome:OnCreated()
+	if self:GetAbility() then
+		self.bonus_all_stats = self:GetAbility():GetSpecialValueFor("bonus_all_stats")
+		self.status_resistance = self:GetAbility():GetSpecialValueFor("status_resistance")
+		self.spell_amp = self:GetAbility():GetSpecialValueFor("spell_amp")
+		self.bonus_base_damage_percent = self:GetAbility():GetSpecialValueFor("bonus_base_damage_percent")
+		self.bonus_cast_range = self:GetAbility():GetSpecialValueFor("bonus_cast_range")
+	end
+end
+
+function modifier_luoshu_tome:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+		MODIFIER_PROPERTY_STATUS_RESISTANCE_STACKING,
+		MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
+		MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE,
+		MODIFIER_PROPERTY_CAST_RANGE_BONUS_STACKING,
+	}
+end
+
+function modifier_luoshu_tome:GetModifierBonusStats_Strength()
+	return self.bonus_all_stats
+end
+
+function modifier_luoshu_tome:GetModifierBonusStats_Agility()
+	return self.bonus_all_stats
+end
+
+function modifier_luoshu_tome:GetModifierBonusStats_Intellect()
+	return self.bonus_all_stats
+end
+
+function modifier_luoshu_tome:GetModifierStatusResistanceStacking()
+	return self.status_resistance
+end
+
+function modifier_luoshu_tome:GetModifierSpellAmplify_Percentage()
+	return self.spell_amp
+end
+
+function modifier_luoshu_tome:GetModifierBaseDamageOutgoing_Percentage()
+	return self.bonus_base_damage_percent
+end
+
+function modifier_luoshu_tome:GetModifierCastRangeBonusStacking()
+	return self.bonus_cast_range
+end
+
+function modifier_luoshu_tome:GetTexture()
+	return "item_tome_of_luoshu"
+end
