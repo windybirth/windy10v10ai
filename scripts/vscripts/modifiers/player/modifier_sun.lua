bd677bdb45f1122058c734ef68fbd8cba1ffc670
windy <windybirth@gmail.com>
2022-07-03 12:40:41 +0900

    add player buff

--- scripts/vscripts/modifiers/player/modifier_sun.lua
+++ scripts/vscripts/modifiers/player/modifier_sun.lua
@@ -2,7 +2,7 @@ modifier_sun = class({})
 
 function modifier_sun:IsPurgable() return false end
 function modifier_sun:RemoveOnDeath() return false end
-function modifier_sun:GetTexture() return "player/plusIcon" end
+function modifier_sun:GetTexture() return "player/sun" end
 
 function modifier_sun:OnCreated()
 	self.iCooldownReduction = 25
