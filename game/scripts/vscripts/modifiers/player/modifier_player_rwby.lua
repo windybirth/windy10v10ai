ddcc671229c4e8d45a9a2c76d453229b29054a00
windy <windybirth@gmail.com>
2022-08-07 12:02:40 +0900

    player buff

--- game/scripts/vscripts/modifiers/player/modifier_player_rwby.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_rwby.lua
@@ -5,15 +5,20 @@ function modifier_player_rwby:RemoveOnDeath() return false end
 function modifier_player_rwby:GetTexture() return "player/rwby" end
 
 function modifier_player_rwby:OnCreated()
-	self.iMoveSpeed = 50
+	self.iMoveSpeed = 100
+	self.iCooldownReduction = 18
 end
 function modifier_player_rwby:DeclareFunctions()
 	return {
 		MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
+		MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
 	}
 end
 
-
 function modifier_player_rwby:GetModifierMoveSpeedBonus_Constant()
 	return self.iMoveSpeed
+end
+
+function modifier_player_rwby:GetModifierPercentageCooldown()
+	return self.iCooldownReduction
 end
