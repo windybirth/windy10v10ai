989550cf96d0fc66955d0e1c8d38bc9a6c2273bb
windy <windybirth@gmail.com>
2022-08-17 01:00:10 +0900

    团队之手给队友奖励

--- game/scripts/vscripts/items/item_hand_of_group.lua
+++ game/scripts/vscripts/items/item_hand_of_group.lua
@@ -1,7 +1,6 @@
 item_hand_of_group=class({})
 
 LinkLuaModifier("modifier_item_hand_of_group_pa", "items/item_hand_of_group.lua", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_item_hand_of_group_buff", "items/item_hand_of_group.lua", LUA_MODIFIER_MOTION_NONE)
 function item_hand_of_group:GetIntrinsicModifierName()
     return "modifier_item_hand_of_group_pa"
 end
@@ -10,17 +9,33 @@ function item_hand_of_group:OnSpellStart()
 	local caster = self:GetCaster()
     local target = self:GetCursorTarget()
     local target_pos = target:GetAbsOrigin()
-    local gold =self:GetSpecialValueFor("gold")
-        caster:EmitSound( "DOTA_Item.Hand_Of_Midas" )
-        local pfx= ParticleManager:CreateParticle("particles/items2_fx/hand_of_midas.vpcf", PATTACH_ABSORIGIN,target)
-        ParticleManager:SetParticleControl(pfx, 0, target_pos)
-        ParticleManager:SetParticleControl(pfx, 1, target_pos)
-        ParticleManager:ReleaseParticleIndex( pfx )
-        target:ForceKill(false)
-        caster:ModifyGold( gold , true, DOTA_ModifyGold_Unspecified )
-        caster:AddExperience( target:GetDeathXP()*self:GetSpecialValueFor("xp") , DOTA_ModifyXP_Unspecified, false, false )
-        SendOverheadEventMessage(caster, OVERHEAD_ALERT_GOLD, caster,gold, caster)
-        caster:AddNewModifier(caster, self, "modifier_item_hand_of_group_buff", {duration=50})
+    local self_xp =self:GetSpecialValueFor("self_xp")
+    local self_gold =self:GetSpecialValueFor("self_gold")
+    local group_xp =self:GetSpecialValueFor("group_xp")
+    local group_gold =self:GetSpecialValueFor("group_gold")
+
+    local pfx= ParticleManager:CreateParticle("particles/items2_fx/hand_of_midas.vpcf", PATTACH_ABSORIGIN,target)
+    ParticleManager:SetParticleControl(pfx, 0, target_pos)
+    ParticleManager:SetParticleControl(pfx, 1, target_pos)
+    ParticleManager:ReleaseParticleIndex( pfx )
+
+    target:ForceKill(false)
+    caster:EmitSound( "DOTA_Item.Hand_Of_Midas" )
+    caster:ModifyGoldFiltered( self_gold , true, DOTA_ModifyGold_Unspecified )
+    caster:AddExperience( target:GetDeathXP() * self_xp , DOTA_ModifyXP_Unspecified, false, false )
+    SendOverheadEventMessage(caster, OVERHEAD_ALERT_GOLD, target, self_gold, caster)
+
+    -- 团队增益
+    local team = caster:GetTeamNumber()
+    local all = FindUnitsInRadius(team, target_pos, nil, self:GetSpecialValueFor("group_range"), DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+    for _,teammate in pairs(all) do
+        if teammate:GetTeamNumber() == team then
+            teammate:EmitSound( "DOTA_Item.Hand_Of_Midas" )
+            teammate:ModifyGoldFiltered( group_gold , true, DOTA_ModifyGold_Unspecified )
+            teammate:AddExperience( target:GetDeathXP() * group_xp , DOTA_ModifyXP_Unspecified, false, false )
+            SendOverheadEventMessage(teammate, OVERHEAD_ALERT_GOLD, teammate, group_gold, caster)
+        end
+    end
 end
 
 modifier_item_hand_of_group_pa=class({})
@@ -37,6 +52,12 @@ function modifier_item_hand_of_group_pa:IsPurgeException()
     return false
 end
 
+function modifier_item_hand_of_group_pa:OnCreated()
+    if self:GetAbility() then
+        self.attack_speed = self:GetAbility():GetSpecialValueFor("attack_speed")
+    end
+end
+
 function modifier_item_hand_of_group_pa:DeclareFunctions()
     return
     {
@@ -45,22 +66,5 @@ function modifier_item_hand_of_group_pa:DeclareFunctions()
 end
 
 function modifier_item_hand_of_group_pa:GetModifierAttackSpeedBonus_Constant()
-    if  self:GetAbility() then
-        return self:GetAbility():GetSpecialValueFor("attsp")
-    end
-    return 0
-end
-
-modifier_item_hand_of_group_buff=class({})
-
-function modifier_item_hand_of_group_buff:IsHidden()
-    return true
-end
-
-function modifier_item_hand_of_group_buff:IsPurgable()
-    return false
-end
-
-function modifier_item_hand_of_group_buff:IsPurgeException()
-    return false
+    return self.attack_speed
 end
