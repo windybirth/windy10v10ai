2e968b22f4bbd60467f59663f785e84e9f9eb2d9
windy <windybirth@gmail.com>
2022-08-16 22:34:38 +0900

    player buff

--- game/scripts/vscripts/modifiers/player/enable_player_modifier.lua
+++ game/scripts/vscripts/modifiers/player/enable_player_modifier.lua
@@ -148,6 +148,10 @@ local yangqiSteamAccountID = Set {
 	-- QQ 杨奇
 	338188516,
 }
+local kudoSteamAccountID = Set {
+	-- kud
+	118324486,
+}
 
 -- 称号属性 END
 
@@ -297,5 +301,9 @@ function EnablePlayerModifier(hEntity)
 	if yangqiSteamAccountID[steamAccountID] then
 		LinkLuaModifier("modifier_player_yangqi", "modifiers/player/modifier_player_yangqi", LUA_MODIFIER_MOTION_NONE)
 		hEntity:AddNewModifier(hEntity, nil, "modifier_player_yangqi", {})
+	end
+	if kudoSteamAccountID[steamAccountID] then
+		LinkLuaModifier("modifier_player_kudo", "modifiers/player/modifier_player_kudo", LUA_MODIFIER_MOTION_NONE)
+		hEntity:AddNewModifier(hEntity, nil, "modifier_player_kudo", {})
 	end
 end
