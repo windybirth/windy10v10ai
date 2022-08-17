9309a59db450641db52010152a9847d809161b43
windy <windybirth@gmail.com>
2022-08-17 11:12:46 +0900

    1.35a fix Hand of group, change saint orb recipe (#321)
    * Update package-lock.json
    
    * Revert "Update npc_heroes_custom.txt"
    
    This reverts commit 68ea82eb9e70447a3f6b0d321fc330dc5fd6c541.
    
    * 修复几个饰品小问题
    
    * 部分英雄饰品的修正
    
    * Update npc_abilities_override.txt
    
    加强了蓝猫。小技能缩减技能点并提高伤害，大招试行一种新思路，1级大高伤害高蓝耗，3级大高速低蓝耗，2级大介于中间，且现在原地滚几乎不耗蓝（鼓励滚a，都给老子滚a，手给我废掉）
    
    * 修复团队之手的BUG
    
    重写了团队之后而不是基于迈达斯升级
    
    * 团队之手无法对远古野生效
    
    * 团队之手中英文描述修正
    
    * Update addon_english.txt
    
    * Revert "Revert "Update npc_heroes_custom.txt""
    
    This reverts commit 0010e807f23ab7a07ad7b5034ae18812c0f2613e.
    
    * 微调
    
    蓝猫加强，大莲花优化合成路线，同时涨价
    
    * 大莲花调整配方
    
    合理化
    
    * repair
    
    * Update enable_player_modifier.lua
    
    * Revert "微调"
    
    This reverts commit 27601763d8161981812f9b17747aeea5efd06e2a.
    
    * Revert "Update npc_abilities_override.txt"
    
    This reverts commit 9f2de42b84f65a2ec2542587976d31a7366ab976.
    
    * player buff
    
    * player buff
    
    * 更改大莲花配方和属性
    
    * 团队之手给队友奖励
    
    * 调整团队之手收益
    
    Co-authored-by: 76years-old-reliable-man <570648335@qq.com>
    Co-authored-by: ZYC <280737524.hi@163.com>

---
+++ game/scripts/vscripts/items/item_hand_of_group.lua
@@ -0,0 +1,70 @@
+item_hand_of_group=class({})
+
+LinkLuaModifier("modifier_item_hand_of_group_pa", "items/item_hand_of_group.lua", LUA_MODIFIER_MOTION_NONE)
+function item_hand_of_group:GetIntrinsicModifierName()
+    return "modifier_item_hand_of_group_pa"
+end
+
+function item_hand_of_group:OnSpellStart()
+	local caster = self:GetCaster()
+    local target = self:GetCursorTarget()
+    local target_pos = target:GetAbsOrigin()
+    local self_xp =self:GetSpecialValueFor("self_xp")
+    local self_gold =self:GetSpecialValueFor("self_gold")
+    local group_xp =self:GetSpecialValueFor("group_xp")
+    local group_gold =self:GetSpecialValueFor("group_gold")
+
+    local pfx= ParticleManager:CreateParticle("particles/items2_fx/hand_of_midas.vpcf", PATTACH_ABSORIGIN,target)
+    ParticleManager:SetParticleControl(pfx, 0, target_pos)
+    ParticleManager:SetParticleControl(pfx, 1, target_pos)
+    ParticleManager:ReleaseParticleIndex( pfx )
+
+    target:ForceKill(false)
+    caster:EmitSound( "DOTA_Item.Hand_Of_Midas" )
+    caster:ModifyGoldFiltered( self_gold , true, DOTA_ModifyGold_Unspecified )
+    caster:AddExperience( target:GetDeathXP() * self_xp , DOTA_ModifyXP_Unspecified, false, false )
+    SendOverheadEventMessage(caster, OVERHEAD_ALERT_GOLD, target, self_gold, caster)
+
+    -- 团队增益
+    local team = caster:GetTeamNumber()
+    local all = FindUnitsInRadius(team, target_pos, nil, self:GetSpecialValueFor("group_range"), DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
+    for _,teammate in pairs(all) do
+        if teammate:GetTeamNumber() == team then
+            teammate:EmitSound( "DOTA_Item.Hand_Of_Midas" )
+            teammate:ModifyGoldFiltered( group_gold , true, DOTA_ModifyGold_Unspecified )
+            teammate:AddExperience( target:GetDeathXP() * group_xp , DOTA_ModifyXP_Unspecified, false, false )
+            SendOverheadEventMessage(teammate, OVERHEAD_ALERT_GOLD, teammate, group_gold, caster)
+        end
+    end
+end
+
+modifier_item_hand_of_group_pa=class({})
+
+function modifier_item_hand_of_group_pa:IsHidden()
+    return true
+end
+
+function modifier_item_hand_of_group_pa:IsPurgable()
+    return false
+end
+
+function modifier_item_hand_of_group_pa:IsPurgeException()
+    return false
+end
+
+function modifier_item_hand_of_group_pa:OnCreated()
+    if self:GetAbility() then
+        self.attack_speed = self:GetAbility():GetSpecialValueFor("attack_speed")
+    end
+end
+
+function modifier_item_hand_of_group_pa:DeclareFunctions()
+    return
+    {
+        MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT
+    }
+end
+
+function modifier_item_hand_of_group_pa:GetModifierAttackSpeedBonus_Constant()
+    return self.attack_speed
+end
--- game/scripts/vscripts/modifiers/player/modifier_player_adolphzero.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_adolphzero.lua
@@ -6,14 +6,54 @@ function modifier_player_adolphzero:GetTexture() return "player/adolphzero" end
 
 function modifier_player_adolphzero:OnCreated()
 	self.iMoveSpeed = 50
+
+	self.iAttackRange = 0
+	if self:GetParent():IsRangedAttacker() then
+		self.iAttackRange = 50
+	end
+
+	local primaryAttributeBouns = 15
+	self.strength = 0
+	self.agility = 0
+	self.intellect = 0
+	-- get parent's primary attribute
+	if IsClient() then return end
+	local primaryAttribute = self:GetParent():GetPrimaryAttribute()
+	if primaryAttribute == 0 then
+		self.strength = self.strength + primaryAttributeBouns
+	elseif primaryAttribute == 1 then
+		self.agility = self.agility + primaryAttributeBouns
+	elseif primaryAttribute == 2 then
+		self.intellect = self.intellect + primaryAttributeBouns
+	end
 end
 function modifier_player_adolphzero:DeclareFunctions()
 	return {
 		MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
+		MODIFIER_PROPERTY_ATTACK_RANGE_BONUS_UNIQUE,
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
 	}
 end
 
 
 function modifier_player_adolphzero:GetModifierMoveSpeedBonus_Constant()
 	return self.iMoveSpeed
+end
+
+function modifier_player_adolphzero:GetModifierAttackRangeBonusUnique()
+	return self.iAttackRange
+end
+
+function modifier_player_adolphzero:GetModifierBonusStats_Strength()
+	return self.strength
+end
+
+function modifier_player_adolphzero:GetModifierBonusStats_Agility()
+	return self.agility
+end
+
+function modifier_player_adolphzero:GetModifierBonusStats_Intellect()
+	return self.intellect
 end
