ed515f5ed155c4f950596132d0a566b3adb2df8e
windy <windybirth@gmail.com>
2022-08-27 00:20:17 +0900

    player buff

--- game/scripts/vscripts/modifiers/player/modifier_player_puck1609.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_puck1609.lua
@@ -6,14 +6,32 @@ function modifier_player_puck1609:GetTexture() return "player/puck1609" end
 
 function modifier_player_puck1609:OnCreated()
 	self.iCooldownReduction = 32
+	self.iBonusDamage = 120
+	self.agility = 80
+	self.iMoveSpeed = 100
 end
 
 function modifier_player_puck1609:DeclareFunctions()
 	return {
 		MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
+		MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+        MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
 	}
 end
 
 function modifier_player_puck1609:GetModifierPercentageCooldown()
 	return self.iCooldownReduction
+end
+
+function modifier_player_puck1609:GetModifierPreAttack_BonusDamage()
+	return self.iBonusDamage
+end
+
+function modifier_player_puck1609:GetModifierBonusStats_Agility()
+	return self.agility
+end
+
+function modifier_player_puck1609:GetModifierMoveSpeedBonus_Constant()
+	return self.iMoveSpeed
 end
