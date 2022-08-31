0b11b4e509a7f3d561c9566e4a54f617109bceb3
windy <windybirth@gmail.com>
2022-08-31 22:48:32 +0900

    player buff

---
+++ game/scripts/vscripts/modifiers/player/modifier_player_puck1609_2.lua
@@ -0,0 +1,9 @@
+modifier_player_puck1609_2 = class({})
+
+function modifier_player_puck1609_2:IsPurgable() return false end
+function modifier_player_puck1609_2:RemoveOnDeath() return false end
+function modifier_player_puck1609_2:GetTexture() return "player/puck1609_2" end
+
+-- 仅图标 属性在 modifier_player_puck1609
+function modifier_player_puck1609_2:OnCreated()
+end
