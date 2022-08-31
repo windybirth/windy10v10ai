20a52ac3465e3646038ad6ba0b536fcc3ca3406b
windy <windybirth@gmail.com>
2022-09-01 02:06:00 +0900

    设定抽选物品列表和爆率 #334

--- game/scripts/vscripts/lottery/items.lua
+++ game/scripts/vscripts/lottery/items.lua
@@ -1,88 +1,74 @@
 --[[ items.lua ]]
 
 function AIGameMode:SpecialItemAdd(owner)
+	local tierRate = {}
+	tierRate[1] = 100
+	tierRate[2] = 20
+	tierRate[3] = 5
+
 	local tier = {}
 
 	tier[1] =	{
+		"item_crown",					-- 王冠
+		"item_belt_of_strength",		-- 力量+6
+		"item_boots_of_elves",			-- 敏捷+6
+		"item_robe",					-- 智力+6
+		"item_magic_wand",				-- 魔棒
+		"item_bracer",					-- 护腕
+		"item_wraith_band",				-- 系带
+		"item_null_talisman",			-- 挂件
+		"item_quelling_blade_2_datadriven",		-- 毒瘤之刃
+
 		"item_ironwood_tree",			-- 铁树之木
+	}
+
+	tier[2] =	{
+		"item_ogre_axe",				-- 力量+10
+		"item_blade_of_alacrity",		-- 敏捷+10
+		"item_staff_of_wizardry",		-- 智力+10
+		"item_orb_of_corrosion",		-- 腐蚀之球
+		"item_urn_of_shadows",			-- 骨灰
+		"item_lifesteal",				-- 吸血面具
+		"item_medallion_of_courage",	-- 勇气勋章
+		"item_falcon_blade",			-- 猎鹰战刃
+
 		"item_ocean_heart",				-- 海洋之心
 		"item_broom_handle",			-- 扫帚柄
 		"item_trusty_shovel",			-- 可靠铁铲
 		"item_faded_broach",			-- 暗淡胸针
-		"item_arcane_ring",				-- 奥术指环
-		"item_winter_embrace",
-		"item_ogre_seal_totem",
-		"item_oblivions_locket",
-		"item_precious_egg",
-		"item_mysterious_hat",
-		"item_possessed_mask",
-		"item_chipped_vest",
-		"item_grove_bow",
-		"item_ring_of_aquila",
-		"item_pupils_gift",
-		"item_imp_claw",
-		"item_misericorde",
-		"item_philosophers_stone",
-		"item_nether_shawl",
-		"item_dragon_scale",
-		"item_essence_ring",
-		"item_vambrace",
-		"item_dimensional_doorway",
-		"item_ambient_sorcery",
-		"item_bear_cloak",
-		"item_bogduggs_baldric",
-		"item_bogduggs_cudgel",
-		"item_bogduggs_lucky_femur",
-		"item_lifestone",
-		"item_pelt_of_the_old_wolf",
-		"item_sign_of_the_arachnid",
-		"item_quicksilver_amulet",
-		"item_bullwhip",
-		"item_paintball",
-	}
-
-	tier[2] =	{
-		"item_quickening_charm",
-		"item_mind_breaker",
-		"item_spider_legs",
-		"item_enchanted_quiver",
-		"item_paladin_sword",
-		"item_orb_of_destruction",
-		"item_black_powder_bag",
-		"item_titan_sliver",
-		"item_horizon",					-- 视界
-		"item_gravel_foot",
-		"item_preserved_skull",
-		"item_slippers_of_the_abyss",
-		"item_treads_of_ermacor",
-		"item_wand_of_the_brine",
-		"item_watchers_gaze",
-		"item_stony_coat",
-		"item_elven_tunic",
-		"item_cloak_of_flames",
-		"item_psychic_headband",
-		"item_ceremonial_robe",
+		"item_keen_optic",				-- 基恩镜片
+		"item_dimensional_doorway",		-- 空间之门
 	}
 
 	tier[3] =	{
-		"item_spy_gadget",
-		"item_timeless_relic",
-		"item_spell_prism",
-		"item_flicker",
-		"item_ninja_gear",
-		"item_illusionsts_cape",
-		"item_guardian_shell",
-		"item_ice_dragon_maw",
-		"item_paw_of_lucius",
-		"item_stonework_pendant",
-		"item_unhallowed_icon",
-		"item_carapace_of_qaldin",
-		"item_rhyziks_eye",
-		"item_the_leveller",
-		"item_penta_edged_sword",
-		"item_stormcrafter",
-		"item_trickster_cloak",
-		"item_heavy_blade",
+		"item_hand_of_midas",			-- 点金手
+		"item_holy_locket",				-- 圣洁吊坠
+
+		"item_quicksilver_amulet",		-- 银闪护符
+		"item_spider_legs",				-- 网虫腿
+		"item_essence_ring",			-- 精华指环
+		"item_nether_shawl",			-- 幽冥披风
+		"item_pupils_gift",				-- 学徒之礼
+		"item_horizon",					-- 视界
+
+		-- "item_spy_gadget",
+		-- "item_timeless_relic",
+		-- "item_spell_prism",
+		-- "item_flicker",
+		-- "item_ninja_gear",
+		-- "item_illusionsts_cape",
+		-- "item_guardian_shell",
+		-- "item_ice_dragon_maw",
+		-- "item_paw_of_lucius",
+		-- "item_stonework_pendant",
+		-- "item_unhallowed_icon",
+		-- "item_carapace_of_qaldin",
+		-- "item_rhyziks_eye",
+		-- "item_the_leveller",
+		-- "item_penta_edged_sword",
+		-- "item_stormcrafter",
+		-- "item_trickster_cloak",
+		-- "item_heavy_blade",
 	}
 
 	tier[4] =	{
@@ -114,9 +100,16 @@ function AIGameMode:SpecialItemAdd(owner)
 	local item_tier = 1
 
 	local spawnedItem = {}
-	for i = 1, 4 do
-		-- test
-		item_tier = i
+	for i = 1, 3 do
+		local draw = RandomFloat(0.0, 100.0)
+		for iTier=#tierRate, 1, -1 do
+			if draw < tierRate[iTier] then
+				item_tier = iTier
+				break
+			end
+		end
+		print("Draw rate "..draw.."Tier "..item_tier)
+
 
 		while true do
 			local repeated_item = false
