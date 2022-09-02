7fa0fdc2ac31eadb701e3be61a0755bc8400f942
windy <windybirth@gmail.com>
2022-09-03 00:55:34 +0900

    player buff

--- game/scripts/vscripts/modifiers/player/modifier_player_kdvh_1.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_kdvh_1.lua
@@ -2,7 +2,7 @@ modifier_player_kdvh_1 = class({})
 
 function modifier_player_kdvh_1:IsPurgable() return false end
 function modifier_player_kdvh_1:RemoveOnDeath() return false end
-function modifier_player_kdvh_1:GetTexture() return "player/plusIcon" end
+function modifier_player_kdvh_1:GetTexture() return "player/kdvh_1" end
 
 function modifier_player_kdvh_1:OnCreated()
 	self.iCooldownReduction = 8
