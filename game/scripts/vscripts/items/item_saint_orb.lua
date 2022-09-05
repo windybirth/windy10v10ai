2eb30ff5984a4a752413ee989cf97a92d7666232
76years-old-reliable-man <570648335@qq.com>
2022-09-06 03:45:02 +0800

    尽力了，整了俩小时整不出减物品cd

--- game/scripts/vscripts/items/item_saint_orb.lua
+++ game/scripts/vscripts/items/item_saint_orb.lua
@@ -33,6 +33,7 @@ function modifier_item_saint_orb_passive:DeclareFunctions() return
 		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
 		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
 		MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+		MODIFIER_PROPERTY_ITEM_COOLDOWN_PERCENTAGE,
 	}
 end
 function modifier_item_saint_orb_passive:CheckState()
@@ -76,6 +77,9 @@ function modifier_item_saint_orb_passive:GetModifierBonusStats_Agility()
 end
 function modifier_item_saint_orb_passive:GetModifierBonusStats_Intellect()
 	return self.bonus_all_stats
+end
+function modifier_item_saint_orb_passove:GetModifierItemPercentageCooldown()
+	return self.iItemCooldownReduction
 end
 
 modifier_item_saint_orb_buff = class({})
