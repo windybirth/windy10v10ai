74ecd66ec51df4a32386ae17612fe106c94e8bff
windy <windybirth@gmail.com>
2022-09-10 16:04:19 +0900

    恢复76的改动

--- game/scripts/vscripts/modifiers/player/modifier_player_76.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_76.lua
@@ -13,10 +13,25 @@ end
 function modifier_player_76:DeclareFunctions()
 	return {
 		MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
+		MODIFIER_PROPERTY_MANA_REGEN_TOTAL_PERCENTAGE,
+		MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
+		MODIFIER_EVENT_ON_ATTACK_LANDED,
 	}
 end
 
 
 function modifier_player_76:GetModifierMoveSpeedBonus_Constant()
 	return self.iMoveSpeed
+end
+
+function modifier_player_76:GetModifierTotalPercentageManaRegen()
+	return self.Bonus_Mana_Regen_Total_Percentage
+end
+
+function modifier_player_76:GetModifierPercentageCooldown()
+	return self.iCooldownReduction
+end
+
+function modifier_player_76:OnAttackLanded(params)
+	LifeStealOnAttackLanded(params, self.iLifeSteal)
 end
