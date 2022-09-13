1bb9a7a85c2035cb65f22c4a5f273d4c43988f61
76years-old-reliable-man <570648335@qq.com>
2022-09-13 20:45:46 +0800

    76玩家buff内容简化统一

--- game/scripts/vscripts/modifiers/player/modifier_player_76.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_76.lua
@@ -6,7 +6,7 @@ function modifier_player_76:GetTexture() return "player/76" end
 
 function modifier_player_76:OnCreated()
 	self.iMoveSpeed = 176
-	self.Bonus_Mana_Regen_Total_Percentage = 1.76
+	self.iManaRegenPercent = 1.76
 	self.iCooldownReduction = 17.6
 	self.iLifeSteal = 17.6
 end
@@ -27,7 +27,7 @@ function modifier_player_76:GetModifierMoveSpeedBonus_Constant()
 end
 
 function modifier_player_76:GetModifierTotalPercentageManaRegen()
-	return self.Bonus_Mana_Regen_Total_Percentage
+	return self.iManaRegenPercent
 end
 
 function modifier_player_76:GetModifierPercentageCooldown()
