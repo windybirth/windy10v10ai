66ff1ac174305c7b844713a5612c17aeb3843595
76years-old-reliable-man <570648335@qq.com>
2022-09-13 20:42:14 +0800

    玩家定制buff完善

--- game/scripts/vscripts/modifiers/player/modifier_player_Asprose.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_Asprose.lua
@@ -2,7 +2,7 @@ modifier_player_asprose = class({})
 
 function modifier_player_asprose:IsPurgable() return false end
 function modifier_player_asprose:RemoveOnDeath() return false end
-function modifier_player_asprose:GetTexture() return "player/Asprose" end
+function modifier_player_asprose:GetTexture() return "player/asprose" end
 
 function modifier_player_asprose:OnCreated()
 	self.iMoveSpeed = 50
