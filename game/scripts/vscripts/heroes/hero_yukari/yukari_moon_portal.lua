759c7b79ad6b925e31634bf451c31f36a86f936f
windy <windybirth@gmail.com>
2022-09-16 13:28:57 +0900

    八云紫3技能不释放2段时 不进入CD #395

--- game/scripts/vscripts/heroes/hero_yukari/yukari_moon_portal.lua
+++ game/scripts/vscripts/heroes/hero_yukari/yukari_moon_portal.lua
@@ -143,7 +143,7 @@ function modifier_yukari_moon_portal_caster:OnDestroy()
 
     self.ability = self:GetAbility()
     self.parent = self:GetParent()
-    --self.ability:StartCooldown(self.ability:GetCooldown(-1) * self.parent:GetCooldownReduction())
+    self.ability:StartCooldown(self.ability:GetCooldown(-1) * self.parent:GetCooldownReduction()-self.ability:GetSpecialValueFor("enemy_lift_duration"))
 
     local HiddenAbilities = {
         "yukari_moon_portal",
