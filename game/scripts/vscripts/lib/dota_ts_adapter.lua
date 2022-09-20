feda484d52ce8a0cce5ac19ac07e8fb6b21a2d1f
windy <windybirth@gmail.com>
2022-09-21 01:28:28 +0900

    #341 导入ts-to-lua框架

---
+++ game/scripts/vscripts/lib/dota_ts_adapter.lua
@@ -0,0 +1,131 @@
+local ____lualib = require("lualib_bundle")
+local __TS__Class = ____lualib.__TS__Class
+local __TS__ClassExtends = ____lualib.__TS__ClassExtends
+local __TS__Delete = ____lualib.__TS__Delete
+local __TS__SourceMapTraceBack = ____lualib.__TS__SourceMapTraceBack
+__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["8"] = 117,["9"] = 117,["10"] = 118,["11"] = 119,["12"] = 120,["13"] = 121,["14"] = 122,["15"] = 122,["16"] = 122,["17"] = 122,["19"] = 125,["22"] = 129,["23"] = 130,["24"] = 130,["25"] = 131,["26"] = 132,["27"] = 135,["28"] = 136,["31"] = 140,["34"] = 2,["35"] = 2,["36"] = 2,["38"] = 2,["39"] = 5,["40"] = 5,["41"] = 5,["43"] = 5,["44"] = 8,["45"] = 8,["46"] = 8,["48"] = 8,["49"] = 9,["50"] = 16,["51"] = 9,["52"] = 21,["53"] = 21,["54"] = 21,["55"] = 21,["56"] = 24,["57"] = 24,["58"] = 24,["59"] = 24,["60"] = 27,["61"] = 27,["62"] = 27,["63"] = 27,["64"] = 30,["65"] = 31,["66"] = 32,["67"] = 34,["68"] = 35,["69"] = 37,["71"] = 39,["73"] = 42,["74"] = 44,["75"] = 46,["76"] = 48,["77"] = 49,["78"] = 50,["79"] = 51,["80"] = 52,["82"] = 49,["83"] = 34,["84"] = 57,["85"] = 58,["86"] = 60,["88"] = 62,["90"] = 65,["91"] = 66,["92"] = 68,["93"] = 70,["94"] = 72,["95"] = 73,["96"] = 74,["97"] = 75,["98"] = 76,["100"] = 73,["101"] = 80,["102"] = 81,["103"] = 82,["104"] = 83,["105"] = 84,["107"] = 86,["108"] = 87,["110"] = 89,["111"] = 90,["114"] = 94,["116"] = 97,["117"] = 57,["120"] = 104,["121"] = 105,["122"] = 106,["123"] = 107,["124"] = 106,["125"] = 104,["126"] = 111,["127"] = 112,["128"] = 113,["130"] = 111});
+local ____exports = {}
+local getFileScope, toDotaClassInstance
+function getFileScope(self)
+    local level = 1
+    while true do
+        local info = debug.getinfo(level, "S")
+        if info and info.what == "main" then
+            return {
+                getfenv(level),
+                info.source
+            }
+        end
+        level = level + 1
+    end
+end
+function toDotaClassInstance(self, instance, ____table)
+    local ____table_0 = ____table
+    local prototype = ____table_0.prototype
+    while prototype do
+        for key in pairs(prototype) do
+            if not (rawget(instance, key) ~= nil) then
+                instance[key] = prototype[key]
+            end
+        end
+        prototype = getmetatable(prototype)
+    end
+end
+____exports.BaseAbility = __TS__Class()
+local BaseAbility = ____exports.BaseAbility
+BaseAbility.name = "BaseAbility"
+function BaseAbility.prototype.____constructor(self)
+end
+____exports.BaseItem = __TS__Class()
+local BaseItem = ____exports.BaseItem
+BaseItem.name = "BaseItem"
+function BaseItem.prototype.____constructor(self)
+end
+____exports.BaseModifier = __TS__Class()
+local BaseModifier = ____exports.BaseModifier
+BaseModifier.name = "BaseModifier"
+function BaseModifier.prototype.____constructor(self)
+end
+function BaseModifier.apply(self, target, caster, ability, modifierTable)
+    return target:AddNewModifier(caster, ability, self.name, modifierTable)
+end
+____exports.BaseModifierMotionHorizontal = __TS__Class()
+local BaseModifierMotionHorizontal = ____exports.BaseModifierMotionHorizontal
+BaseModifierMotionHorizontal.name = "BaseModifierMotionHorizontal"
+__TS__ClassExtends(BaseModifierMotionHorizontal, ____exports.BaseModifier)
+____exports.BaseModifierMotionVertical = __TS__Class()
+local BaseModifierMotionVertical = ____exports.BaseModifierMotionVertical
+BaseModifierMotionVertical.name = "BaseModifierMotionVertical"
+__TS__ClassExtends(BaseModifierMotionVertical, ____exports.BaseModifier)
+____exports.BaseModifierMotionBoth = __TS__Class()
+local BaseModifierMotionBoth = ____exports.BaseModifierMotionBoth
+BaseModifierMotionBoth.name = "BaseModifierMotionBoth"
+__TS__ClassExtends(BaseModifierMotionBoth, ____exports.BaseModifier)
+setmetatable(____exports.BaseAbility.prototype, {__index = CDOTA_Ability_Lua or C_DOTA_Ability_Lua})
+setmetatable(____exports.BaseItem.prototype, {__index = CDOTA_Item_Lua or C_DOTA_Item_Lua})
+setmetatable(____exports.BaseModifier.prototype, {__index = CDOTA_Modifier_Lua or C_DOTA_Modifier_Lua})
+____exports.registerAbility = function(____, name) return function(____, ability)
+    if name ~= nil then
+        ability.name = name
+    else
+        name = ability.name
+    end
+    local env = unpack(getFileScope(nil))
+    env[name] = {}
+    toDotaClassInstance(nil, env[name], ability)
+    local originalSpawn = env[name].Spawn
+    env[name].Spawn = function(self)
+        self:____constructor()
+        if originalSpawn then
+            originalSpawn(self)
+        end
+    end
+end end
+____exports.registerModifier = function(____, name) return function(____, modifier)
+    if name ~= nil then
+        modifier.name = name
+    else
+        name = modifier.name
+    end
+    local env, source = unpack(getFileScope(nil))
+    local fileName = string.gsub(source, ".*scripts[\\/]vscripts[\\/]", "")
+    env[name] = {}
+    toDotaClassInstance(nil, env[name], modifier)
+    local originalOnCreated = env[name].OnCreated
+    env[name].OnCreated = function(self, parameters)
+        self:____constructor()
+        if originalOnCreated then
+            originalOnCreated(self, parameters)
+        end
+    end
+    local ____type = LUA_MODIFIER_MOTION_NONE
+    local base = modifier.____super
+    while base do
+        if base == ____exports.BaseModifierMotionBoth then
+            ____type = LUA_MODIFIER_MOTION_BOTH
+            break
+        elseif base == ____exports.BaseModifierMotionHorizontal then
+            ____type = LUA_MODIFIER_MOTION_HORIZONTAL
+            break
+        elseif base == ____exports.BaseModifierMotionVertical then
+            ____type = LUA_MODIFIER_MOTION_VERTICAL
+            break
+        end
+        base = base.____super
+    end
+    LinkLuaModifier(name, fileName, ____type)
+end end
+--- Use to expose top-level functions in entity scripts.
+-- Usage: registerEntityFunction("OnStartTouch", (trigger: TriggerStartTouchEvent) => { <your code here> });
+function ____exports.registerEntityFunction(self, name, f)
+    local env = unpack(getFileScope(nil))
+    env[name] = function(...)
+        f(nil, ...)
+    end
+end
+local function clearTable(self, ____table)
+    for key in pairs(____table) do
+        __TS__Delete(____table, key)
+    end
+end
+return ____exports
