a3c7c960efcd2a397bd32d8a73a55558b7a7b1d8
puck1609 <1609578991@qq.com>
2022-09-23 22:18:40 +0800

    fix #406
     1.ug
     2.修复 npc_heroes_custom.txt 重复kv
     3.萨尔小改动
     4.剧毒

---
+++ game/scripts/vscripts/heroes/hero_spectre/spectre_ursa_enrage.lua
@@ -0,0 +1,56 @@
+LinkLuaModifier("modifier_spectre_ursa_enrage", "heroes/hero_spectre/spectre_ursa_enrage", LUA_MODIFIER_MOTION_NONE)
+
+spectre_ursa_enrage = class({})
+
+function spectre_ursa_enrage:OnSpellStart()
+    self.caster = self:GetCaster()
+    print('spectre_ursa_enrage duration:')
+    self.duration = self:GetSpecialValueFor('duration')
+    print(self.duration)
+    self.caster:EmitSound('Hero_Spectre.Haunt')
+    self.caster:AddNewModifier(self.caster, self, "modifier_spectre_ursa_enrage", { duration = self.duration })
+end
+
+modifier_spectre_ursa_enrage = class({})
+function modifier_spectre_ursa_enrage:IsHidden() return false end
+function modifier_spectre_ursa_enrage:IsDebuff() return false end
+function modifier_spectre_ursa_enrage:IsPurgable() return false end
+function modifier_spectre_ursa_enrage:IsPurgeException() return false end
+function modifier_spectre_ursa_enrage:RemoveOnDeath() return true end
+function modifier_spectre_ursa_enrage:AllowIllusionDuplicate() return false end
+function modifier_spectre_ursa_enrage:GetTexture() return "spe_new_ultimate" end
+
+function modifier_spectre_ursa_enrage:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE,
+        MODIFIER_PROPERTY_STATUS_RESISTANCE_STACKING,
+    }
+end
+
+function modifier_spectre_ursa_enrage:GetModifierIncomingDamage_Percentage()
+    return self.damageReduction
+end
+
+function modifier_spectre_ursa_enrage:GetModifierStatusResistanceStacking()
+    return self.statusResistance
+end
+
+function modifier_spectre_ursa_enrage:OnCreated(table)
+    self.ability = self:GetAbility()
+    self.caster = self:GetParent()
+    self.damageReduction = self.ability:GetSpecialValueFor('damage_reduction')
+    self.statusResistance = self.ability:GetSpecialValueFor('status_resistance')
+    print('spectre_ursa_enrage damageReduction & statusResistance:')
+    print(self.damageReduction)
+    print(self.statusResistance)
+    self.particleId = ParticleManager:CreateParticle("particles/yukari_true_moon.vpcf", PATTACH_ABSORIGIN_FOLLOW, self.caster)
+end
+
+function modifier_spectre_ursa_enrage:OnRefresh(table)
+    self:OnCreated(table)
+end
+
+function modifier_spectre_ursa_enrage:OnDestroy()
+    ParticleManager:DestroyParticle(self.particleId, true)
+    ParticleManager:ReleaseParticleIndex(self.particleId)
+end
--- game/scripts/vscripts/modifiers/player/modifier_player_puck1609_4.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_puck1609_4.lua
@@ -3,6 +3,7 @@ modifier_player_puck1609_4 = class({})
 function modifier_player_puck1609_4:IsPurgable() return false end
 function modifier_player_puck1609_4:RemoveOnDeath() return false end
 function modifier_player_puck1609_4:GetTexture() return "player/puck1609_4" end
+function modifier_player_puck1609:IsHidden() return true end
 
 -- 仅图标 属性在 modifier_player_puck1609
 function modifier_player_puck1609_4:OnCreated()
