46093f4fe1d91b2aa6293904cf08378132416f16
puck1609 <1609578991@qq.com>
2022-10-09 22:25:26 +0800

    个性化测试环境 debug

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -250,12 +250,13 @@ function AIGameMode:PreGameOptions()
 
     self.PreGameOptionsSet = true
 
-    self.ApplyTestOptions()
+    self:ApplyTestOptions()
 
 end
 
 -- 个性化测试环境
 function AIGameMode:ApplyTestOptions()
+    print('------------------------读取个性化测试环境------------------------')
     if self.DebugMode and PlayerResource:GetSteamAccountID(0) == 245559423 then
         self.iDesiredRadiant = 2
         self.iDesiredDire = 2
