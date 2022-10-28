8911d0b6ac420ba5119cf786d65bcfee3fa2fcb0
puck1609 <1609578991@qq.com>
2022-10-28 13:31:23 +0800

    fix #145

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -241,6 +241,15 @@ function AIGameMode:PreGameOptions()
 
 end
 
+-- 个性化测试环境
+function AIGameMode:ApplyTestOptions()
+    print('------------------------读取个性化测试环境------------------------')
+    if self.DebugMode and PlayerResource:GetSteamAccountID(0) == 245559423 then
+        self.iDesiredRadiant = 10
+        self.iDesiredDire = 10
+    end
+end
+
 ------------------------------------------------------------------
 --                        Gold/XP Filter                        --
 ------------------------------------------------------------------
--- game/scripts/vscripts/custom_test_env.lua
+++
@@ -1,8 +0,0 @@
--- 个性化测试环境
-function AIGameMode:ApplyTestOptions()
-    print('------------------------读取个性化测试环境------------------------')
-    if self.DebugMode and PlayerResource:GetSteamAccountID(0) == 245559423 then
-        self.iDesiredRadiant = 10
-        self.iDesiredDire = 10
-    end
-end
