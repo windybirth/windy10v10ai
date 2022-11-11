9fe7351e174a8d26bd9cc6562ae86f01dce73768
76years-old-reliable-man <570648335@qq.com>
2022-11-11 22:00:54 +0800

    refresher

--- game/scripts/vscripts/items/item_wizard_refresher.lua
+++ game/scripts/vscripts/items/item_wizard_refresher.lua
@@ -1,20 +1,68 @@
-modifier_item_wizard_refresher = class{()}
+item_wizard_refresher = class({
+    GetIntrinsicModifierName = function()
+        return "modifier_item_refresher_wizard"
+    end
+})
 
-function item_wizard_refresher:GetIntrinsicModifierName()
-    return "modifier_item_wizard_refresher"
+function item_wizard_refresher:GetCooldown(iLevel)
+    local caster = self:GetCaster()
+    local baseCooldown = self.BaseClass.GetCooldown(self, iLevel)
+    return baseCooldown / caster:GetCooldownReduction()
+end
+
+function item_wizard_refresher:IsRefreshable()
+    return false
+end
+
+function item_wizard_refresherb:OnSpellStart()
+    if(not IsServer()) then
+        return
+    end
+    local caster = self:GetCaster()
+    EmitSoundOn("RefresherCore.Activate", caster)
+
+    local nFXIndex = ParticleManager:CreateParticle("particles/items2_fx/refresher.vpcf", PATTACH_CUSTOMORIGIN, caster)
+    ParticleManager:SetParticleControlEnt(nFXIndex, 0, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetOrigin(), true)
+    ParticleManager:ReleaseParticleIndex(nFXIndex, 2)
+
+    for i=0, DOTA_MAX_ABILITIES-1, 1 do
+        local current_ability = caster:GetAbilityByIndex(i)
+        if current_ability ~= nil then
+            if current_ability:IsRefreshable() then
+                current_ability:RefreshCharges()
+                current_ability:EndCooldown()
+            end
+        end
+    end
+    for i=0, DOTA_ITEM_MAX-1, 1 do
+        local current_item = caster:GetItemInSlot(i)
+        if current_item ~= nil and current_item:IsRefreshable() then
+            current_item:EndCooldown()
+        end
+    end
 end
 
 modifier_item_wizard_refresher = class({
+    IsHidden = function()
+        return true
+    end,
+    IsPurgable = function()
+        return false
+    end,
+    IsPurgeException = function()
+        return false
+    end,
+	GetAttributes = function()
+        return MODIFIER_ATTRIBUTE_MULTIPLE
+    end,
 	RegisterFunctions = function()
         return
         {
-            MODIFIER_PROPERTY_ROSHDEF_BONUS_HEALTH,
             MODIFIER_PROPERTY_MANA_BONUS,
-            MODIFIER_PROPERTY_EXTRA_MANA_BONUS,
-            MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+            MODIFIER_PROPERTY_STATS_STREGTH_BONUS,
             MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
-            MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
-            MODIFIER_PROPERTY_COOLDOWN_REDUCTION
+            MODIFIER_PROPERTY_STATS_STREGTH_BONUS,
+            MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
         }
     end
 })
@@ -30,28 +78,12 @@ function modifier_item_wizard_refresher:OnRefresh()
     if(not self.ability or self.ability:IsNull() == true) then
         return
     end
-    self.bonus_health = self.ability:GetSpecialValueFor("bonus_health")
     self.bonus_mana = self.ability:GetSpecialValueFor("bonus_mana")
     self.bonus_allstats = self.ability:GetSpecialValueFor("bonus_allstats")
-    self.cooldown_reduction = self.ability:GetSpecialValueFor("cooldown_reduction")
-end
-
-function modifier_item_wizard_refresher:GetModifierBonusHealth()
-		return self.bonus_health
 end
 
 function modifier_item_wizard_refresher:GetModifierManaBonus()
-	if(self.parent:IsRealHero() == true) then
-		return self.bonus_mana
-	end
-	return 0
-end
-
-function modifier_item_wizard_refresher:GetModifierExtraManaBonus()
-	if(self.parent:IsRealHero() == true) then
-		return 0
-	end
-	return self.bonus_mana
+    return self.bonus_mana
 end
 
 function modifier_item_wizard_refresher:GetModifierBonusStats_Strength()
@@ -66,8 +98,8 @@ function modifier_item_wizard_refresher:GetModifierBonusStats_Intellect()
     return self.bonus_allstats
 end
 
-function modifier_item_wizard_refresher:GetModifierPercentageCooldownStackingUnique()
-	return self.cooldown_reduction
+function modifier_item_wizard_refresher:GetModifierPercentageCooldown()
+    return self.cooldown_reduction
 end
 
 LinkLuaModifier("modifier_item_wizard_refresher", "items/item_wizard_refresher", LUA_MODIFIER_MOTION_NONE, modifier_item_wizard_refresher)
