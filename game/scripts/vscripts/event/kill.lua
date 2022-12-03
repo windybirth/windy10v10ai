f31170c7952b5f2ff8b70c5072008ca29996b4dd
windy <windybirth@gmail.com>
2022-12-03 14:58:20 +0900

    fix bot multi bug

--- game/scripts/vscripts/event/kill.lua
+++ game/scripts/vscripts/event/kill.lua
@@ -271,7 +271,7 @@ local function HeroKilled(keys)
             teamKillFactor = 4
         end
         extraFactor = extraFactor * teamKillFactor
-        extraFactor = extraFactor * 10 / AIGameMode.playerNumber
+        extraFactor = extraFactor * AIGameMode.playerNumber / 10
 
         gold = gold * extraFactor
         xp = xp * AIGameMode:GetPlayerGoldXpMultiplier(playerId) * extraFactor
