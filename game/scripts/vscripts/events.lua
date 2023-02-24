8ff4e93a3296a7e033947c71ee3881d01e94e3b8
windy <windybirth@gmail.com>
2023-02-24 17:34:12 +0900

    更新N6阵容 #580

--- game/scripts/vscripts/events.lua
+++ game/scripts/vscripts/events.lua
@@ -3,128 +3,7 @@ require('event/js_event')
 require('event/creep')
 require('event/chat')
 require('event/kill')
-
-local tBotNameList = {
-    --"npc_dota_hero_invoker",
-    --"npc_dota_hero_antimage", // 不会放技能，只会物品和A人
-    --"npc_dota_hero_spirit_breaker", // 不会放技能，只会物品和A人
-    --"npc_dota_hero_silencer", // 不会放技能，只会物品和A人
-    --"npc_dota_hero_mirana", // 不会放技能，只会物品和A人
-    --"npc_dota_hero_furion", // 不会放技能，只会物品和A人
-    --"npc_dota_hero_huskar", // 不会放技能，只会物品和A人
-    --"npc_dota_hero_batrider",
-    --"npc_dota_hero_obsidian_destroyer",
-    --"npc_dota_hero_enchantress",
-    --"npc_dota_hero_snapfire",
-    --"npc_dota_hero_broodmother",
-    --"npc_dota_hero_lycan",
-    --"npc_dota_hero_arc_warden",
-    --"npc_dota_hero_ancient_apparition",
-    --"npc_dota_hero_treant",
-    --"npc_dota_hero_rubick",
-    --"npc_dota_hero_shredder",
-    "npc_dota_hero_tinker",
-    "npc_dota_hero_abaddon",
-    "npc_dota_hero_axe",
-    "npc_dota_hero_bane",
-    "npc_dota_hero_bounty_hunter",
-    "npc_dota_hero_bloodseeker",
-    "npc_dota_hero_bristleback",
-    "npc_dota_hero_chaos_knight",
-    "npc_dota_hero_crystal_maiden",
-    "npc_dota_hero_dazzle",
-    "npc_dota_hero_death_prophet",
-    "npc_dota_hero_dragon_knight",
-    "npc_dota_hero_drow_ranger",
-    "npc_dota_hero_earthshaker",
-    "npc_dota_hero_jakiro",
-    "npc_dota_hero_juggernaut",
-    "npc_dota_hero_kunkka",
-    "npc_dota_hero_lich",
-    "npc_dota_hero_lina",
-    "npc_dota_hero_lion",
-    "npc_dota_hero_luna",
-    "npc_dota_hero_medusa",
-    "npc_dota_hero_meepo",
-    "npc_dota_hero_necrolyte",
-    "npc_dota_hero_nevermore",
-    "npc_dota_hero_ogre_magi",
-    "npc_dota_hero_omniknight",
-    "npc_dota_hero_oracle",
-    "npc_dota_hero_phantom_assassin",
-    "npc_dota_hero_pudge",
-    "npc_dota_hero_riki",
-    --"npc_dota_hero_razor", // 在泉水站着完全不动
-    "npc_dota_hero_sand_king",
-    "npc_dota_hero_shadow_shaman",
-    "npc_dota_hero_skywrath_mage",
-    "npc_dota_hero_sniper",
-    "npc_dota_hero_spectre",
-    "npc_dota_hero_sven",
-    --"npc_dota_hero_tidehunter", // 在泉水站着完全不动
-    "npc_dota_hero_tiny",
-    "npc_dota_hero_vengefulspirit",
-    "npc_dota_hero_viper",
-    "npc_dota_hero_warlock",
-    "npc_dota_hero_windrunner",
-    "npc_dota_hero_witch_doctor",
-    "npc_dota_hero_skeleton_king",
-    "npc_dota_hero_zuus",
-}
-
-
-local tBotAllStar = {
-    "npc_dota_hero_abaddon",
-    "npc_dota_hero_spectre",
-    "npc_dota_hero_dragon_knight",
-    "npc_dota_hero_necrolyte",
-    "npc_dota_hero_omniknight",
-}
-
-local tBotAllStarRandom = {
-    team1 = {
-        "npc_dota_hero_bristleback",
-        "npc_dota_hero_viper",
-        "npc_dota_hero_sniper",
-        "npc_dota_hero_zuus",
-        "npc_dota_hero_nevermore",
-    },
-    team2 = {
-        "npc_dota_hero_sniper",
-        "npc_dota_hero_zuus",
-        "npc_dota_hero_phantom_assassin",
-        "npc_dota_hero_riki",
-        "npc_dota_hero_bloodseeker",
-    },
-    team3 = {
-        "npc_dota_hero_bristleback",
-        "npc_dota_hero_viper",
-        "npc_dota_hero_earthshaker",
-        "npc_dota_hero_axe",
-        "npc_dota_hero_medusa",
-    },
-    team4 = {
-        "npc_dota_hero_viper",
-        "npc_dota_hero_nevermore",
-        "npc_dota_hero_luna",
-        "npc_dota_hero_medusa",
-        "npc_dota_hero_drow_ranger",
-    },
-    team5 = {
-        "npc_dota_hero_warlock",
-        "npc_dota_hero_dazzle",
-        "npc_dota_hero_oracle",
-        "npc_dota_hero_viper",
-        "npc_dota_hero_medusa",
-    },
-    team6 = {
-        "npc_dota_hero_bristleback",
-        "npc_dota_hero_viper",
-        "npc_dota_hero_sniper",
-        "npc_dota_hero_zuus",
-        "npc_dota_hero_phantom_assassin",
-    },
-}
+require('event/bot_herolist')
 
 local tSkillCustomNameList = { "npc_dota_hero_crystal_maiden", "npc_dota_hero_queenofpain", "npc_dota_hero_mirana",
                                "npc_dota_hero_earthshaker", "npc_dota_hero_nevermore" }
@@ -188,7 +67,9 @@ function AIGameMode:InitHeroSelection()
 
         if self.iGameDifficulty == 6 then
             print("[AIGameMode] Use all star hero list start")
-            for _, v in ipairs(tBotAllStarRandom["team" .. tostring(math.random(1, 6))]) do
+            local iRandomTeam = math.random(1, 11)
+            print("[AIGameMode] Random team: " .. tostring(iRandomTeam))
+            for _, v in ipairs(tBotAllStarRandom["team" .. tostring(iRandomTeam)]) do
                 table.insert(tBotAllStar, v)
             end
             self:ArrayShuffle(tBotAllStar)
