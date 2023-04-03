ed3975090fb5cb5376edce7d7fe541dfc50d1671
windy <windybirth@gmail.com>
2023-04-03 23:47:14 +0900

    hjauin icon

---
+++ game/scripts/vscripts/modifiers/player/modifier_player_hjauin.lua
@@ -0,0 +1,5 @@
+modifier_player_hjauin = class({})
+
+function modifier_player_hjauin:IsPurgable() return false end
+function modifier_player_hjauin:RemoveOnDeath() return false end
+function modifier_player_hjauin:GetTexture() return "player/hjauin" end
