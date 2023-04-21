ab754b875ae31906f0736b8e3dbffd4e19aab39c
windy <windybirth@gmail.com>
2023-04-21 22:35:04 +0900

    fix icon

--- game/scripts/vscripts/modifiers/player/modifier_player_xingguang.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_xingguang.lua
@@ -2,4 +2,4 @@ modifier_player_xingguang = class({})
 
 function modifier_player_xingguang:IsPurgable() return false end
 function modifier_player_xingguang:RemoveOnDeath() return false end
-function modifier_player_xingguang:GetTexture() return "player/plusIcon" end
+function modifier_player_xingguang:GetTexture() return "player/xingguang" end
