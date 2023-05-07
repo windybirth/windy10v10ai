a366975402d147a1a362a11acdd73347841557e4
windy <windybirth@gmail.com>
2023-05-07 14:01:40 +0900

    Update chat.lua

--- game/scripts/vscripts/event/chat.lua
+++ game/scripts/vscripts/event/chat.lua
@@ -176,19 +176,50 @@ function AIGameMode:OnPlayerChat(event)
             local tAllHeroes = FindUnitsInRadius(DOTA_TEAM_NOTEAM, Vector(0, 0, 0), nil, 99999, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
             for _, hero in pairs(tAllHeroes) do
                 hero:AddNewModifier(hero, nil, modifier, {value = 1})
+                hero:AddNewModifier(hero, nil, modifier, {value = 1})
+                hero:AddNewModifier(hero, nil, modifier, {value = 1})
+                hero:AddNewModifier(hero, nil, modifier, {value = 1})
+                hero:AddNewModifier(hero, nil, modifier, {value = 1})
+                hero:AddNewModifier(hero, nil, modifier, {value = 1})
             end
             return
         end
 
         -- remove all modifier
-        if sChatMsg:find('^-remove modifier all .+') then
-            local modifier = sChatMsg:sub(22)
-            Printf("开发者:" .. developerSteamAccountID[steamAccountID] .. " 给所有人移除modifier:" .. modifier)
+        if sChatMsg:find('^-remove player modifier') then
+            local modiferList = {
+                "property_cooldown_percentage",
+                "property_cast_range_bonus_stacking",
+                "property_spell_amplify_percentage",
+                "property_status_resistance_stacking",
+                "property_magical_resistance_bonus",
+                "property_incoming_damage_percentage",
+                "property_attack_range_bonus",
+                "property_physical_armor_bonus",
+                "property_preattack_bonus_damage",
+                "property_attackspeed_bonus_constant",
+                "property_stats_strength_bonus",
+                "property_stats_agility_bonus",
+                "property_stats_intellect_bonus",
+                "property_health_regen_percentage",
+                "property_mana_regen_total_percentage",
+                "property_lifesteal",
+                "property_spell_lifesteal",
+                "property_movespeed_bonus_constant",
+                "property_ignore_movespeed_limit",
+                "property_cannot_miss",
+            }
+            Printf("开发者:" .. developerSteamAccountID[steamAccountID] .. " 给所有人移除player modifier")
             -- give all player item
             local tAllHeroes = FindUnitsInRadius(DOTA_TEAM_NOTEAM, Vector(0, 0, 0), nil, 99999, DOTA_UNIT_TARGET_TEAM_BOTH, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER, false)
             for _, hero in pairs(tAllHeroes) do
-                if hero:HasModifier(modifier) then
-                    hero:RemoveModifierByName(modifier)
+                local hereModifiers = hero:FindAllModifiers()
+                for _, modifier in pairs(hereModifiers) do
+                    for _, modifierName in pairs(modiferList) do
+                        if modifier:GetName() == modifierName then
+                            hero:RemoveModifierByName(modifierName)
+                        end
+                    end
                 end
             end
             return
