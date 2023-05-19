8ab9f2ad0ae766cc75d23f09e6212719813a4fe5
windy <windybirth@gmail.com>
2023-05-19 23:16:38 +0900

    add icon

---
+++ game/scripts/vscripts/modifiers/player/modifier_player_eesama.lua
@@ -0,0 +1,5 @@
+modifier_player_eesama = class({})
+
+function modifier_player_eesama:IsPurgable() return false end
+function modifier_player_eesama:RemoveOnDeath() return false end
+function modifier_player_eesama:GetTexture() return "player/eesama" end
