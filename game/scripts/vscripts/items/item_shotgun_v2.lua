356df116a408266839fbffff939395339b4f29ce
windy <windybirth@gmail.com>
2023-07-25 22:06:38 +0900

    Revert "v2.20 Time limit enhance, item enhance (#663)"
    This reverts commit 36eb9c4c4883a3cbe725b5e9688aee41c43908a8.

--- game/scripts/vscripts/items/item_shotgun_v2.lua
+++ game/scripts/vscripts/items/item_shotgun_v2.lua
@@ -80,7 +80,7 @@ function modifier_item_shotgun_v2:GetModifierProcAttack_Feedback(keys)
 		end
 	end
 
-	keys.attacker:AddNewModifier(keys.attacker, self:GetAbility(), "modifier_item_shotgun_v2_cooldown", {duration = 0})
+	keys.attacker:AddNewModifier(keys.attacker, self:GetAbility(), "modifier_item_shotgun_v2_cooldown", {duration = 0.1})
 end
 
 if modifier_item_shotgun_v2_cooldown == nil then modifier_item_shotgun_v2_cooldown = class({}) end
--- src/vscripts/api/game.ts
+++ src/vscripts/api/game.ts
@@ -23,7 +23,7 @@ class GameInfo {
 
 export class Game {
 
-	private static VERSION = "v2.20";
+	private static VERSION = "v2.19";
 	constructor() {
 	}
 
