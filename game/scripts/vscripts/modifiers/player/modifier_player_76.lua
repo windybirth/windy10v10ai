58b1002ffa44e192fe12f9593b51e5d199eebd74
76years-old-reliable-man <570648335@qq.com>
2023-07-31 11:38:09 +0800

    测试用

--- game/scripts/vscripts/modifiers/player/modifier_player_76.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_76.lua
@@ -3,3 +3,26 @@ modifier_player_76 = class({})
 function modifier_player_76:IsPurgable() return false end
 function modifier_player_76:RemoveOnDeath() return false end
 function modifier_player_76:GetTexture() return "player/76" end
+
+function modifier_player_76:OnCreated()
+	self.istrength = 200
+end
+
+function modifier_player_76:OnCreated()
+	self.iagility = 120
+end
+
+function modifier_player_76:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+        MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+	}
+end
+
+function modifier_player_76:GetModifierBonusStats_Strength()
+	return self.istrength
+end
+
+function modifier_player_76:GetModifierBonusStats_Agility()
+	return self.iagility
+end
