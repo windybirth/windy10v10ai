8d6bfb9cfd596fc1a67d52cc5a28ce930e4b5436
76years-old-reliable-man <570648335@qq.com>
2023-08-01 20:08:39 +0800

    限时

--- game/scripts/vscripts/modifiers/player/modifier_player_xingguang.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_xingguang.lua
@@ -3,3 +3,19 @@ modifier_player_xingguang = class({})
 function modifier_player_xingguang:IsPurgable() return false end
 function modifier_player_xingguang:RemoveOnDeath() return false end
 function modifier_player_xingguang:GetTexture() return "player/xingguang" end
+
+function modifier_player_xingguang:OnCreated()
+    self.imanaregenpct = 1.2
+end
+
+
+function modifier_player_xingguang:DeclareFunctions()
+	return
+	{
+        MODIFIER_PROPERTY_MANA_REGEN_TOTAL_PERCENTAGE,
+	}
+end
+
+function modifier_player_xingguang:GetModifierTotalPercentageManaRegen()
+	return self.imanaregenpct
+end
