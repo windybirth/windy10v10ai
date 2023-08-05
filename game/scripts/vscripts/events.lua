4de7baeebc36fab9112a6a6a98a36b5a4076dfc7
76years-old-reliable-man <570648335@qq.com>
2023-08-05 22:57:24 +0800

    测试

--- game/scripts/vscripts/events.lua
+++ game/scripts/vscripts/events.lua
@@ -968,7 +968,7 @@ function AIGameMode:FilterSeasonPoint(playerInfo, winnerTeamId)
     elseif difficulty == 5 then
         points = points * 0
     elseif difficulty == 6 then
-        points = points * 0
+        points = points * 500
     end
     return math.ceil(points)
 end
