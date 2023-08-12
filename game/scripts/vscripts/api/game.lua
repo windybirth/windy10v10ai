5b70daa19f41c6eaea2c04d146d10eb61394b7e9
windy <windybirth@gmail.com>
2023-08-12 23:00:41 +0900

    v2.23

--- game/scripts/vscripts/api/game.lua
+++ game/scripts/vscripts/api/game.lua
@@ -68,5 +68,5 @@ function Game.prototype.SendEndGameInfo(self, endData)
     }
     ApiClient:sendWithRetry(apiParameter)
 end
-Game.VERSION = "v2.22"
+Game.VERSION = "v2.23"
 return ____exports
--- src/vscripts/api/game.ts
+++ src/vscripts/api/game.ts
@@ -23,7 +23,7 @@ class GameInfo {
 
 export class Game {
 
-	private static VERSION = "v2.22";
+	private static VERSION = "v2.23";
 	constructor() {
 	}
 
