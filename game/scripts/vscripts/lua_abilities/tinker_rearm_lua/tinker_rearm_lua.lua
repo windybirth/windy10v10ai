f18b2a648664376d42ae8d5f898412e6dc3a0d2e
76years-old-reliable-man <570648335@qq.com>
2023-08-13 14:55:32 +0800

    显示强化（完工）

--- game/scripts/vscripts/lua_abilities/tinker_rearm_lua/tinker_rearm_lua.lua
+++ game/scripts/vscripts/lua_abilities/tinker_rearm_lua/tinker_rearm_lua.lua
@@ -83,6 +83,8 @@ function tinker_rearm_lua:IsAbitilyException( ability )
 end
 tinker_rearm_lua.AbitilyException = {
 	["zuus_cloud"] = true,
+	["invoker_sun_strike"] = true,
+}
 }
 function tinker_rearm_lua:IsItemException( item )
 	return self.ItemException[item:GetName()]
