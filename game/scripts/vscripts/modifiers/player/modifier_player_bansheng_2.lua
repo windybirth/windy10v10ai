c2ad4bdcf8ad29fae48d6bf0f358f579fedc9d44
windy <windybirth@gmail.com>
2023-08-20 22:52:46 +0900

    add icon

---
+++ game/scripts/vscripts/modifiers/player/modifier_player_bansheng_2.lua
@@ -0,0 +1,5 @@
+modifier_player_bansheng_2 = class({})
+
+function modifier_player_bansheng_2:IsPurgable() return false end
+function modifier_player_bansheng_2:RemoveOnDeath() return false end
+function modifier_player_bansheng_2:GetTexture() return "player/bansheng_2" end
