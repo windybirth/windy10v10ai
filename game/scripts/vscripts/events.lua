387e458a6fbe3922acf65e1143388e0566056cd0
76years-old-reliable-man <111288032+76years-old-reliable-man@users.noreply.github.com>
2023-09-30 13:52:26 +0800

    限时强化 (#692)
    * 限时强化
    
    * Fix N6 difficulty
    
    ---------
    
    Co-authored-by: windy <windybirth@gmail.com>

--- game/scripts/vscripts/events.lua
+++ game/scripts/vscripts/events.lua
@@ -983,7 +983,7 @@ function AIGameMode:FilterSeasonPoint(playerInfo, winnerTeamId)
     elseif difficulty == 5 then
         points = points * 2.0
     elseif difficulty == 6 then
-        points = points * 2.0
+        points = points * 3.0
     end
     return math.ceil(points)
 end
