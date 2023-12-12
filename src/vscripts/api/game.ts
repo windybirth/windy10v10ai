74c16a77c0a2054979a3af35f2fb5598f21bdfb6
windy <windybirth@gmail.com>
2023-12-13 02:33:07 +0900

    fix ts files

--- src/vscripts/api/game.ts
+++ src/vscripts/api/game.ts
@@ -1,72 +1,83 @@
-
 import { ApiClient, HttpMethod } from "./api_client";
 
+// FIXME
+class EndGameData {
+  players!: { [key: number]: { points: number; isDisconnect: boolean } };
+  winnerTeamId!: number;
+  gameOption!: object;
+}
+
 class Player {
-	teamId!: number;
-	steamId!: number;
-	heroName!: string;
-	points!: number;
-	isDisconnect!: boolean;
+  teamId!: number;
+  steamId!: number;
+  heroName!: string;
+  points!: number;
+  isDisconnect!: boolean;
 }
 
 class GameInfo {
-	players!: Player[];
-	winnerTeamId!: number;
-	matchId!: string;
-	gameOption!: Object;
-	version!: string;
-	constructor() {
-		print("[Game] constructor in TS");
-		this.players = [];
-	}
+  players!: Player[];
+  winnerTeamId!: number;
+  matchId!: string;
+  gameOption!: object;
+  version!: string;
+  constructor() {
+    print("[Game] constructor in TS");
+    this.players = [];
+  }
 }
 
 export class Game {
+  private static VERSION = "v3.01";
+  constructor() {}
 
-	private static VERSION = "v3.01";
-	constructor() {
-	}
+  public SendEndGameInfo(endData: EndGameData) {
+    if (
+      GetDedicatedServerKeyV2(ApiClient.SERVER_KEY) == ApiClient.LOCAL_APIKEY &&
+      !IsInToolsMode()
+    ) {
+      return;
+    }
 
-	public SendEndGameInfo(endData: any) {
+    CustomNetTables.SetTableValue("ending_status", "ending_status", {
+      status: 1,
+    });
 
-		if (GetDedicatedServerKeyV2(ApiClient.SERVER_KEY) == ApiClient.LOCAL_APIKEY && !IsInToolsMode()) {
-			return;
-		}
+    const gameInfo = new GameInfo();
+    gameInfo.winnerTeamId = endData.winnerTeamId;
+    gameInfo.matchId = GameRules.Script_GetMatchID().toString();
+    gameInfo.version = Game.VERSION;
+    gameInfo.gameOption = endData.gameOption;
 
-		CustomNetTables.SetTableValue("ending_status", "ending_status", { status: 1 });
+    for (let i = 0; i < PlayerResource.GetPlayerCount(); i++) {
+      if (PlayerResource.IsValidPlayerID(i)) {
+        const player = new Player();
+        player.teamId = PlayerResource.GetTeam(i);
+        player.steamId = PlayerResource.GetSteamAccountID(i);
+        player.heroName = PlayerResource.GetSelectedHeroName(i);
+        player.points = endData.players[i]?.points;
+        player.isDisconnect = endData.players[i]?.isDisconnect;
+        gameInfo.players.push(player);
+      }
+    }
+    const apiParameter = {
+      method: HttpMethod.POST,
+      path: ApiClient.POST_GAME_URL,
+      body: gameInfo,
+      successFunc: (data: string) => {
+        CustomNetTables.SetTableValue("ending_status", "ending_status", {
+          status: 2,
+        });
+        print(`[Game] end game callback data ${data}`);
+      },
+      failureFunc: (data: string) => {
+        CustomNetTables.SetTableValue("ending_status", "ending_status", {
+          status: 3,
+        });
+        print(`[Game] end game callback data ${data}`);
+      },
+    };
 
-		const gameInfo = new GameInfo();
-		gameInfo.winnerTeamId = endData.winnerTeamId;
-		gameInfo.matchId = GameRules.Script_GetMatchID().toString();
-		gameInfo.version = Game.VERSION;
-		gameInfo.gameOption = endData.gameOption;
-
-		for (let i = 0; i < PlayerResource.GetPlayerCount(); i++) {
-			if (PlayerResource.IsValidPlayerID(i)) {
-				const player = new Player();
-				player.teamId = PlayerResource.GetTeam(i);
-				player.steamId = PlayerResource.GetSteamAccountID(i);
-				player.heroName = PlayerResource.GetSelectedHeroName(i);
-				player.points = endData.players[i]?.points;
-				player.isDisconnect = endData.players[i]?.isDisconnect;
-				gameInfo.players.push(player);
-			}
-		}
-		const apiParameter = {
-			method: HttpMethod.POST,
-			path: ApiClient.POST_GAME_URL,
-			body: gameInfo,
-			successFunc: (data: string) => {
-				CustomNetTables.SetTableValue("ending_status", "ending_status", { status: 2 });
-				print(`[Game] end game callback data ${data}`);
-			},
-			failureFunc: (data: string) => {
-				CustomNetTables.SetTableValue("ending_status", "ending_status", { status: 3 });
-				print(`[Game] end game callback data ${data}`);
-			},
-		};
-
-		ApiClient.sendWithRetry(apiParameter);
-	}
-
+    ApiClient.sendWithRetry(apiParameter);
+  }
 }
--- src/vscripts/modifiers/property/property_controller.ts
+++ src/vscripts/modifiers/property/property_controller.ts
@@ -1,102 +1,189 @@
 import { PlayerProperty } from "../../api/player";
-import { property_attackspeed_bonus_constant, property_attack_range_bonus, property_cannot_miss, property_cast_range_bonus_stacking, property_cooldown_percentage, property_health_regen_percentage, property_ignore_movespeed_limit, property_incoming_damage_percentage, property_lifesteal, property_magical_resistance_bonus, property_mana_regen_total_percentage, property_movespeed_bonus_constant, property_physical_armor_bonus, property_preattack_bonus_damage, property_spell_amplify_percentage, property_spell_lifesteal, property_stats_agility_bonus, property_stats_intellect_bonus, property_stats_strength_bonus, property_status_resistance_stacking, property_evasion_constant } from "./property_declare";
+import {
+  property_attackspeed_bonus_constant,
+  property_attack_range_bonus,
+  property_cannot_miss,
+  property_cast_range_bonus_stacking,
+  property_cooldown_percentage,
+  property_health_regen_percentage,
+  property_ignore_movespeed_limit,
+  property_incoming_damage_percentage,
+  property_lifesteal,
+  property_magical_resistance_bonus,
+  property_mana_regen_total_percentage,
+  property_movespeed_bonus_constant,
+  property_physical_armor_bonus,
+  property_preattack_bonus_damage,
+  property_spell_amplify_percentage,
+  property_spell_lifesteal,
+  property_stats_agility_bonus,
+  property_stats_intellect_bonus,
+  property_stats_strength_bonus,
+  property_status_resistance_stacking,
+  property_evasion_constant,
+} from "./property_declare";
 
 export class PropertyController {
-    private static propertyValuePerLevel = new Map<string, number>();
-    private static propertyDataDrivenName = new Map<string, string>();
-    constructor() {
-        print("PropertyController init");
-        PropertyController.propertyValuePerLevel.set(property_cooldown_percentage.name, 4);
-        PropertyController.propertyValuePerLevel.set(property_cast_range_bonus_stacking.name, 25);
-        PropertyController.propertyValuePerLevel.set(property_spell_amplify_percentage.name, 5);
-        PropertyController.propertyValuePerLevel.set(property_status_resistance_stacking.name, 4);
-        PropertyController.propertyValuePerLevel.set(property_evasion_constant.name, 4);
-        PropertyController.propertyValuePerLevel.set(property_magical_resistance_bonus.name, 4);
-        PropertyController.propertyValuePerLevel.set(property_incoming_damage_percentage.name, -4);
-        PropertyController.propertyValuePerLevel.set(property_attack_range_bonus.name, 25);
-        PropertyController.propertyValuePerLevel.set(property_health_regen_percentage.name, 0.3);
-        PropertyController.propertyValuePerLevel.set(property_mana_regen_total_percentage.name, 0.3);
-        PropertyController.propertyValuePerLevel.set(property_lifesteal.name, 10);
-        PropertyController.propertyValuePerLevel.set(property_spell_lifesteal.name, 8);
-        PropertyController.propertyValuePerLevel.set(property_ignore_movespeed_limit.name, 0.125);
-        PropertyController.propertyValuePerLevel.set(property_cannot_miss.name, 0.125);
+  private static propertyValuePerLevel = new Map<string, number>();
+  private static propertyDataDrivenName = new Map<string, string>();
+  constructor() {
+    print("PropertyController init");
+    PropertyController.propertyValuePerLevel.set(
+      property_cooldown_percentage.name,
+      4
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_cast_range_bonus_stacking.name,
+      25
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_spell_amplify_percentage.name,
+      5
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_status_resistance_stacking.name,
+      4
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_evasion_constant.name,
+      4
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_magical_resistance_bonus.name,
+      4
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_incoming_damage_percentage.name,
+      -4
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_attack_range_bonus.name,
+      25
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_health_regen_percentage.name,
+      0.3
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_mana_regen_total_percentage.name,
+      0.3
+    );
+    PropertyController.propertyValuePerLevel.set(property_lifesteal.name, 10);
+    PropertyController.propertyValuePerLevel.set(
+      property_spell_lifesteal.name,
+      8
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_ignore_movespeed_limit.name,
+      0.125
+    );
+    PropertyController.propertyValuePerLevel.set(
+      property_cannot_miss.name,
+      0.125
+    );
 
-        // multi level property must end with '_level_'
-        PropertyController.propertyDataDrivenName.set(
-            property_movespeed_bonus_constant.name, 'modifier_player_property_movespeed_bonus_constant_level_'
-        );
+    // multi level property must end with '_level_'
+    PropertyController.propertyDataDrivenName.set(
+      property_movespeed_bonus_constant.name,
+      "modifier_player_property_movespeed_bonus_constant_level_"
+    );
 
-        PropertyController.propertyDataDrivenName.set(
-            property_physical_armor_bonus.name, 'modifier_player_property_physical_armor_bonus_level_'
+    PropertyController.propertyDataDrivenName.set(
+      property_physical_armor_bonus.name,
+      "modifier_player_property_physical_armor_bonus_level_"
+    );
+    PropertyController.propertyDataDrivenName.set(
+      property_preattack_bonus_damage.name,
+      "modifier_player_property_preattack_bonus_damage_level_"
+    );
+    PropertyController.propertyDataDrivenName.set(
+      property_attackspeed_bonus_constant.name,
+      "modifier_player_property_attackspeed_bonus_constant_level_"
+    );
+    PropertyController.propertyDataDrivenName.set(
+      property_stats_strength_bonus.name,
+      "modifier_player_property_stats_strength_bonus_level_"
+    );
+    PropertyController.propertyDataDrivenName.set(
+      property_stats_agility_bonus.name,
+      "modifier_player_property_stats_agility_bonus_level_"
+    );
+    PropertyController.propertyDataDrivenName.set(
+      property_stats_intellect_bonus.name,
+      "modifier_player_property_stats_intellect_bonus_level_"
+    );
+  }
+
+  public static addModifier(
+    hero: CDOTA_BaseNPC_Hero,
+    property: PlayerProperty
+  ) {
+    const propertyValuePerLevel = PropertyController.propertyValuePerLevel.get(
+      property.name
+    );
+    if (propertyValuePerLevel) {
+      const value = propertyValuePerLevel * property.level;
+      if (value != 0) {
+        hero.RemoveModifierByName(property.name);
+        hero.AddNewModifier(hero, undefined, property.name, {
+          value,
+        });
+      }
+    } else {
+      const dataDrivenName = PropertyController.propertyDataDrivenName.get(
+        property.name
+      );
+      if (dataDrivenName) {
+        this.refreshDataDrivenPlayerProperty(
+          hero,
+          dataDrivenName,
+          property.level
         );
-        PropertyController.propertyDataDrivenName.set(
-            property_preattack_bonus_damage.name, 'modifier_player_property_preattack_bonus_damage_level_'
-        );
-        PropertyController.propertyDataDrivenName.set(
-            property_attackspeed_bonus_constant.name, 'modifier_player_property_attackspeed_bonus_constant_level_'
-        );
-        PropertyController.propertyDataDrivenName.set(
-            property_stats_strength_bonus.name, 'modifier_player_property_stats_strength_bonus_level_'
-        );
-        PropertyController.propertyDataDrivenName.set(
-            property_stats_agility_bonus.name, 'modifier_player_property_stats_agility_bonus_level_'
-        );
-        PropertyController.propertyDataDrivenName.set(
-            property_stats_intellect_bonus.name, 'modifier_player_property_stats_intellect_bonus_level_'
-        );
+      }
+    }
+  }
+
+  public static RefreshPlayerProperty(property: PlayerProperty) {
+    for (let i = 0; i < PlayerResource.GetPlayerCount(); i++) {
+      if (PlayerResource.IsValidPlayer(i)) {
+        const steamId = PlayerResource.GetSteamAccountID(i);
+        if (steamId == property.steamId) {
+          const hero = PlayerResource.GetSelectedHeroEntity(i);
+          if (hero) {
+            PropertyController.addModifier(hero, property);
+          }
+        }
+      }
+    }
+  }
+
+  private static refreshDataDrivenPlayerProperty(
+    hero: CDOTA_BaseNPC_Hero,
+    dataDrivenName: string,
+    level: number
+  ) {
+    if (level == 0) {
+      return;
     }
 
-    public static addModifier(hero: CDOTA_BaseNPC_Hero, property: PlayerProperty) {
-        const propertyValuePerLevel = PropertyController.propertyValuePerLevel.get(property.name);
-        if (propertyValuePerLevel) {
-            const value = propertyValuePerLevel * property.level;
-            if (value != 0) {
-                hero.RemoveModifierByName(property.name);
-                hero.AddNewModifier(hero, undefined, property.name, {
-                    value
-                });
-            }
-        } else {
-            const dataDrivenName = PropertyController.propertyDataDrivenName.get(property.name);
-            if (dataDrivenName) {
-                this.refreshDataDrivenPlayerProperty(hero, dataDrivenName, property.level);
-            }
-        }
+    if (dataDrivenName.endsWith("_level_")) {
+      // for 1-8 level
+      for (let i = 1; i <= 8; i++) {
+        hero.RemoveModifierByName(`${dataDrivenName}${i}`);
+      }
+      dataDrivenName = dataDrivenName + level;
+    } else {
+      hero.RemoveModifierByName(dataDrivenName);
     }
 
-    public static RefreshPlayerProperty(property: PlayerProperty) {
-        const steamId = property.steamId;
-
-        for (let i = 0; i < PlayerResource.GetPlayerCount(); i++) {
-            if (PlayerResource.IsValidPlayer(i)) {
-                const steamId = PlayerResource.GetSteamAccountID(i);
-                if (steamId == property.steamId) {
-                    const hero = PlayerResource.GetSelectedHeroEntity(i);
-                    if (hero) {
-                        PropertyController.addModifier(hero, property);
-                    }
-                }
-            }
-        }
-    }
-
-    private static refreshDataDrivenPlayerProperty(hero: CDOTA_BaseNPC_Hero, dataDrivenName: string, level: number) {
-        if (level == 0) {
-            return;
-        }
-
-        if (dataDrivenName.endsWith('_level_')) {
-            // for 1-8 level
-            for (let i = 1; i <= 8; i++) {
-                hero.RemoveModifierByName(`${dataDrivenName}${i}`);
-            }
-            dataDrivenName = dataDrivenName + level;
-        } else {
-            hero.RemoveModifierByName(dataDrivenName);
-        }
-
-        const modifierItem = CreateItem("item_player_modifiers", undefined, undefined) as CDOTA_Item_DataDriven;
-        modifierItem.ApplyDataDrivenModifier(hero, hero, dataDrivenName, { duration: -1 });
-        UTIL_RemoveImmediate(modifierItem);
-    }
+    const modifierItem = CreateItem(
+      "item_player_modifiers",
+      undefined,
+      undefined
+    ) as CDOTA_Item_DataDriven;
+    modifierItem.ApplyDataDrivenModifier(hero, hero, dataDrivenName, {
+      duration: -1,
+    });
+    UTIL_RemoveImmediate(modifierItem);
+  }
 }
