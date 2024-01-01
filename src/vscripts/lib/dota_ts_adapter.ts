c214d85f258f17368442e4d11d7ff656ba527829
windy <windybirth@gmail.com>
2024-01-01 19:51:38 +0900

    Update dota_ts_adapter.ts

--- src/vscripts/lib/dota_ts_adapter.ts
+++ src/vscripts/lib/dota_ts_adapter.ts
@@ -1,42 +1,43 @@
-export interface BaseAbility extends CDOTA_Ability_Lua { }
-export class BaseAbility { }
+export interface BaseAbility extends CDOTA_Ability_Lua {}
+export class BaseAbility {}
 
-export interface BaseItem extends CDOTA_Item_Lua { }
-export class BaseItem { }
+export interface BaseItem extends CDOTA_Item_Lua {}
+export class BaseItem {}
 
-export interface BaseModifier extends CDOTA_Modifier_Lua { }
+export interface BaseModifier extends CDOTA_Modifier_Lua {}
 export class BaseModifier {
-    public static apply<T extends typeof BaseModifier>(
-        this: T,
-        target: CDOTA_BaseNPC,
-        caster?: CDOTA_BaseNPC,
-        ability?: CDOTABaseAbility,
-        modifierTable?: object,
-    ): InstanceType<T> {
-        return target.AddNewModifier(caster, ability, this.name, modifierTable) as any;
-    }
+  public static apply<T extends typeof BaseModifier>(
+    this: T,
+    target: CDOTA_BaseNPC,
+    caster?: CDOTA_BaseNPC,
+    ability?: CDOTABaseAbility,
+    modifierTable?: object,
+  ): InstanceType<T> {
+    return target.AddNewModifier(caster, ability, this.name, modifierTable) as any;
+  }
 }
 
-export interface BaseModifierMotionHorizontal extends CDOTA_Modifier_Lua_Horizontal_Motion { }
-export class BaseModifierMotionHorizontal extends BaseModifier { }
+export interface BaseModifierMotionHorizontal extends CDOTA_Modifier_Lua_Horizontal_Motion {}
+export class BaseModifierMotionHorizontal extends BaseModifier {}
 
-export interface BaseModifierMotionVertical extends CDOTA_Modifier_Lua_Vertical_Motion { }
-export class BaseModifierMotionVertical extends BaseModifier { }
+export interface BaseModifierMotionVertical extends CDOTA_Modifier_Lua_Vertical_Motion {}
+export class BaseModifierMotionVertical extends BaseModifier {}
 
-export interface BaseModifierMotionBoth extends CDOTA_Modifier_Lua_Motion_Both { }
-export class BaseModifierMotionBoth extends BaseModifier { }
+export interface BaseModifierMotionBoth extends CDOTA_Modifier_Lua_Motion_Both {}
+export class BaseModifierMotionBoth extends BaseModifier {}
 
 // Add standard base classes to prototype chain to make `super.*` work as `self.BaseClass.*`
 setmetatable(BaseAbility.prototype, { __index: CDOTA_Ability_Lua ?? C_DOTA_Ability_Lua });
 setmetatable(BaseItem.prototype, { __index: CDOTA_Item_Lua ?? C_DOTA_Item_Lua });
 setmetatable(BaseModifier.prototype, { __index: CDOTA_Modifier_Lua ?? CDOTA_Modifier_Lua });
 
-export const registerAbility = (name?: string) => (ability: new () => CDOTA_Ability_Lua | CDOTA_Item_Lua) => {
+export const registerAbility =
+  (name?: string) => (ability: new () => CDOTA_Ability_Lua | CDOTA_Item_Lua) => {
     if (name !== undefined) {
-        // @ts-ignore
-        ability.name = name;
+      // @ts-ignore
+      ability.name = name;
     } else {
-        name = ability.name;
+      name = ability.name;
     }
 
     const [env] = getFileScope();
@@ -47,54 +48,54 @@ export const registerAbility = (name?: string) => (ability: new () => CDOTA_Abil
 
     const originalSpawn = (env[name] as CDOTA_Ability_Lua).Spawn;
     env[name].Spawn = function () {
-        this.____constructor();
-        if (originalSpawn) {
-            originalSpawn.call(this);
-        }
+      this.____constructor();
+      if (originalSpawn) {
+        originalSpawn.call(this);
+      }
     };
-};
+  };
 
 export const registerModifier = (name?: string) => (modifier: new () => CDOTA_Modifier_Lua) => {
-    if (name !== undefined) {
-        // @ts-ignore
-        modifier.name = name;
-    } else {
-        name = modifier.name;
+  if (name !== undefined) {
+    // @ts-ignore
+    modifier.name = name;
+  } else {
+    name = modifier.name;
+  }
+
+  const [env, source] = getFileScope();
+  const [fileName] = string.gsub(source, ".*scripts[\\/]vscripts[\\/]", "");
+
+  env[name] = {};
+
+  toDotaClassInstance(env[name], modifier);
+
+  const originalOnCreated = (env[name] as CDOTA_Modifier_Lua).OnCreated;
+  env[name].OnCreated = function (parameters: any) {
+    this.____constructor();
+    if (!!originalOnCreated) {
+      originalOnCreated.call(this, parameters);
+    }
+  };
+
+  let type = LuaModifierMotionType.NONE;
+  let base = (modifier as any).____super;
+  while (base) {
+    if (base === BaseModifierMotionBoth) {
+      type = LuaModifierMotionType.BOTH;
+      break;
+    } else if (base === BaseModifierMotionHorizontal) {
+      type = LuaModifierMotionType.HORIZONTAL;
+      break;
+    } else if (base === BaseModifierMotionVertical) {
+      type = LuaModifierMotionType.VERTICAL;
+      break;
     }
 
-    const [env, source] = getFileScope();
-    const [fileName] = string.gsub(source, ".*scripts[\\/]vscripts[\\/]", "");
+    base = base.____super;
+  }
 
-    env[name] = {};
-
-    toDotaClassInstance(env[name], modifier);
-
-    const originalOnCreated = (env[name] as CDOTA_Modifier_Lua).OnCreated;
-    env[name].OnCreated = function (parameters: any) {
-        this.____constructor();
-        if (!!originalOnCreated) {
-            originalOnCreated.call(this, parameters);
-        }
-    };
-
-    let type = LuaModifierMotionType.NONE;
-    let base = (modifier as any).____super;
-    while (base) {
-        if (base === BaseModifierMotionBoth) {
-            type = LuaModifierMotionType.BOTH;
-            break;
-        } else if (base === BaseModifierMotionHorizontal) {
-            type = LuaModifierMotionType.HORIZONTAL;
-            break;
-        } else if (base === BaseModifierMotionVertical) {
-            type = LuaModifierMotionType.VERTICAL;
-            break;
-        }
-
-        base = base.____super;
-    }
-
-    LinkLuaModifier(name, fileName, type);
+  LinkLuaModifier(name, fileName, type);
 };
 
 /**
@@ -102,41 +103,41 @@ export const registerModifier = (name?: string) => (modifier: new () => CDOTA_Mo
  * Usage: registerEntityFunction("OnStartTouch", (trigger: TriggerStartTouchEvent) => { <your code here> });
  */
 export function registerEntityFunction(name: string, f: (...args: any[]) => any) {
-    const [env] = getFileScope();
-    env[name] = function (this: void, ...args: any[]) {
-        f(...args);
-    };
+  const [env] = getFileScope();
+  env[name] = function (this: void, ...args: any[]) {
+    f(...args);
+  };
 }
 
 function clearTable(table: object) {
-    for (const key in table) {
-        delete (table as any)[key];
-    }
+  for (const key in table) {
+    delete (table as any)[key];
+  }
 }
 
 function getFileScope(): [any, string] {
-    let level = 1;
-    while (true) {
-        const info = debug.getinfo(level, "S");
-        if (info && info.what === "main") {
-            return [getfenv(level), info.source!];
-        }
-
-        level += 1;
+  let level = 1;
+  while (true) {
+    const info = debug.getinfo(level, "S");
+    if (info && info.what === "main") {
+      return [getfenv(level), info.source!];
     }
+
+    level += 1;
+  }
 }
 
 function toDotaClassInstance(instance: any, table: new () => any) {
-    let { prototype } = table;
-    while (prototype) {
-        for (const key in prototype) {
-            // Using hasOwnProperty to ignore methods from metatable added by ExtendInstance
-            // https://github.com/SteamDatabase/GameTracking-Dota2/blob/7edcaa294bdcf493df0846f8bbcd4d47a5c3bd57/game/core/scripts/vscripts/init.lua#L195
-            if (!instance.hasOwnProperty(key)) {
-                instance[key] = prototype[key];
-            }
-        }
-
-        prototype = getmetatable(prototype);
+  let { prototype } = table;
+  while (prototype) {
+    for (const key in prototype) {
+      // Using hasOwnProperty to ignore methods from metatable added by ExtendInstance
+      // https://github.com/SteamDatabase/GameTracking-Dota2/blob/7edcaa294bdcf493df0846f8bbcd4d47a5c3bd57/game/core/scripts/vscripts/init.lua#L195
+      if (!instance.hasOwnProperty(key)) {
+        instance[key] = prototype[key];
+      }
     }
+
+    prototype = getmetatable(prototype);
+  }
 }
