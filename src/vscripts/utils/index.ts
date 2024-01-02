4a58821830d2cafd7d6683eb39da4fb5c53e1da7
windy <windybirth@gmail.com>
2024-01-02 10:31:48 +0900

    remove unused file

--- src/vscripts/utils/index.ts
+++ src/vscripts/utils/index.ts
@@ -1,10 +1,5 @@
 // 引入所有的lua模块
-require('aeslua');
-require('decrypt');
-require('json');
-require('md5');
-require('popups');
-require('timers');
-
-// rename SHA and make it global
-globalThis.SHA = require('sha');
+require("json");
+require("md5");
+require("popups");
+require("timers");
