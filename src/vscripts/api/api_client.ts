738c8b292991f8c76c0bb8eca8fc6acd94e9fc9b
windy <windybirth@gmail.com>
2024-01-03 20:54:07 +0900

    Update api_client.ts

--- src/vscripts/api/api_client.ts
+++ src/vscripts/api/api_client.ts
@@ -26,10 +26,11 @@ export class ApiClient {
   private static RETRY_TIMES = 4;
   // dont change this version, it is used to identify the server
   public static SERVER_KEY = "v1.43";
-  private static HOST_NAME: string = (() => {
-    return IsInToolsMode() ? "http://localhost:5000/api" : "https://windy10v10ai.web.app/api";
-  })();
-  // private static HOST_NAME: string = "https://windy10v10ai.web.app/api";
+
+  // private static HOST_NAME: string = (() => {
+  //   return IsInToolsMode() ? "http://localhost:5000/api" : "https://windy10v10ai.web.app/api";
+  // })();
+  private static HOST_NAME: string = "https://windy10v10ai.web.app/api";
 
   public static send(
     method: HttpMethod,
