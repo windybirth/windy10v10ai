3f62f3da398b0df59d015d9581e2807af251a5b8
windy <windybirth@gmail.com>
2024-01-04 14:22:03 +0900

    Update lualib_bundle.lua

--- game/scripts/vscripts/lualib_bundle.lua
+++ game/scripts/vscripts/lualib_bundle.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: inject-field, deprecated, duplicate-set-field
 local function __TS__ArrayIsArray(value)
     return type(value) == "table" and (value[1] ~= nil or next(value) == nil)
 end
