f2acf86cc55f5c383036ce87001cca44ab4729e2
windy <windybirth@gmail.com>
2024-01-07 14:23:55 +0900

    balance

--- game/scripts/vscripts/items/item_vladmir_ultra.lua
+++
@@ -1,8 +0,0 @@
-function VampiricAuraApply(event)
-    -- Variables
-    local attacker = event.attacker
-    local ability = event.inflictor
-    local iLifeSteal = 150
-	event.damage = event.attack_damage
-	LifeStealOnAttackLanded(event, iLifeSteal, attacker, ability)
-end
