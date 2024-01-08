bf85dff87f27ec00ac1a0a30113f0e01d3633919
windy <windybirth@gmail.com>
2024-01-08 16:31:45 +0900

    移除编译后的文件

--- game/scripts/vscripts/server/core/ApiRequestOptions.lua
+++
@@ -1,5 +0,0 @@
-local ____lualib = require("lualib_bundle")
-local __TS__SourceMapTraceBack = ____lualib.__TS__SourceMapTraceBack
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {});
-local ____exports = {}
-return ____exports
--- game/scripts/vscripts/utils/index.lua
+++
@@ -1,7 +0,0 @@
-local ____lualib = require("lualib_bundle")
-local __TS__SourceMapTraceBack = ____lualib.__TS__SourceMapTraceBack
-__TS__SourceMapTraceBack(debug.getinfo(1).short_src, {["4"] = 2,["5"] = 3,["6"] = 4,["7"] = 5});
-require("utils.json")
-require("utils.md5")
-require("utils.popups")
-require("utils.timers")
