3a7d585aae47a5b8856e639ddc9634041796f325
windy <windybirth@gmail.com>
2024-01-17 00:06:39 +0900

    revert 限时神器时代&限时挑战 #740

--- content/panorama/scripts/custom_game/game_mode.js
+++ content/panorama/scripts/custom_game/game_mode.js
@@ -244,7 +244,7 @@ function InitN5Setting() {
 }
 function InitN6Setting() {
   $("#player_gold_xp_multiplier_dropdown").SetSelected("1.5");
-  $("#bot_gold_xp_multiplier_dropdown").SetSelected("15");
+  $("#bot_gold_xp_multiplier_dropdown").SetSelected("10");
 
   $("#tower_power_dropdown").SetSelected("10");
   $("#tower_endure_dropdown").SetSelected("10");
@@ -315,7 +315,7 @@ function OnGameOptionsChange() {
       seasonPointMulti = "2.0";
       break;
     case 6:
-      seasonPointMulti = "2.5";
+      seasonPointMulti = "2.0";
       break;
     default:
       seasonPointMulti = "1.0";
