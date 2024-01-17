6815156e07cbd1c4b2026115e1833d99adfdf9d3
windy <windybirth@gmail.com>
2024-01-17 10:22:07 +0900

    Update game.ts

--- src/vscripts/api/game.ts
+++ src/vscripts/api/game.ts
@@ -31,7 +31,7 @@ class EndGameInfo {
 }
 
 export class Game {
-  private static VERSION = "v3.08";
+  private static VERSION = "v3.09";
   constructor() {}
 
   public SendEndGameInfo(endData: EndGameInfo) {
