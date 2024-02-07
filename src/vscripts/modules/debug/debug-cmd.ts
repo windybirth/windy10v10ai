cb496dfe0f5135fdd676b1630e3d1048a9254ae3
windy <windybirth@gmail.com>
2024-02-08 01:32:03 +0900

    fix

--- src/vscripts/modules/debug/debug-cmd.ts
+++ src/vscripts/modules/debug/debug-cmd.ts
@@ -1,7 +1,7 @@
 // cmd enum
 
 export enum CMD {
-  G = "g", // 加钱升级
-  V = "v", // 获取当前vector
-  REFRESH_AI = "r", // 刷新AI
+  G = "-g", // 加钱升级
+  V = "-v", // 获取当前vector
+  REFRESH_AI = "-r", // 刷新AI
 }
