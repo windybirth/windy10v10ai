343e333abb8ed5d54765e56f3164377e375bce11
windy <windybirth@gmail.com>
2024-02-23 15:44:18 +0900

    balance roshan (#825)
    * balance roshan
    
    * Update npc_abilities_custom.txt
    
    * fix roshan
    
    * Update npc_abilities_custom.txt

--- src/vscripts/modules/event/event-npc-spawned.ts
+++ src/vscripts/modules/event/event-npc-spawned.ts
@@ -85,7 +85,7 @@ export class EventNpcSpawned {
         }
       }
 
-      if (this.roshanLevelBase < 5) {
+      if (this.roshanLevelBase < 5 - 1) {
         this.roshanLevelBase++;
       }
     }
@@ -94,10 +94,16 @@ export class EventNpcSpawned {
   private getExtraRoshanLevel(): number {
     let extra = 0;
 
+    if (Player.GetPlayerCount() >= 2) {
+      extra++;
+    }
     if (Player.GetPlayerCount() >= 4) {
       extra++;
     }
     if (Player.GetPlayerCount() >= 8) {
+      extra++;
+    }
+    if (Player.GetPlayerCount() >= 10) {
       extra++;
     }
     return this.roshanLevelBase + extra;
