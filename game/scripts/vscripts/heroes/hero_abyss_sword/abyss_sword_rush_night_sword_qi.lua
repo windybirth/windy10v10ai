094fd6f17a8ca404a65f3935a72059acb881c441
76years-old-reliable-man <570648335@qq.com>
2024-03-16 22:18:55 +0800

    初音 沉剑吃狂战，兽王土猫墨客大锤加强，紫苑血辣降价，神血辣跟着降价并加强，胸针和神狂战明强暗削，增加一些属性并提高价格

--- game/scripts/vscripts/heroes/hero_abyss_sword/abyss_sword_rush_night_sword_qi.lua
+++ game/scripts/vscripts/heroes/hero_abyss_sword/abyss_sword_rush_night_sword_qi.lua
@@ -1,6 +1,9 @@
-LinkLuaModifier("modifier_abyss_sword_rush_night_sword_qi", "heroes/hero_abyss_sword/abyss_sword_rush_night_sword_qi", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_abyss_sword_rush_night_sword_qi_spell", "heroes/hero_abyss_sword/abyss_sword_rush_night_sword_qi", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_abyss_sword_rush_night_sword_qi_enemy", "heroes/hero_abyss_sword/abyss_sword_rush_night_sword_qi", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_abyss_sword_rush_night_sword_qi", "heroes/hero_abyss_sword/abyss_sword_rush_night_sword_qi",
+    LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_abyss_sword_rush_night_sword_qi_spell",
+    "heroes/hero_abyss_sword/abyss_sword_rush_night_sword_qi", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_abyss_sword_rush_night_sword_qi_enemy",
+    "heroes/hero_abyss_sword/abyss_sword_rush_night_sword_qi", LUA_MODIFIER_MOTION_NONE)
 abyss_sword_rush_night_sword_qi = abyss_sword_rush_night_sword_qi or class({})
 modifier_abyss_sword_rush_night_sword_qi = modifier_abyss_sword_rush_night_sword_qi or class({})
 modifier_abyss_sword_rush_night_sword_qi_spell = modifier_abyss_sword_rush_night_sword_qi_spell or class({})
@@ -10,14 +13,14 @@ function abyss_sword_rush_night_sword_qi:GetCastRange()
     return self:GetSpecialValueFor("length")
 end
 
-function abyss_sword_rush_night_sword_qi:OnProjectileHit(target,location)
+function abyss_sword_rush_night_sword_qi:OnProjectileHit(target, location)
     local caster = self:GetCaster()
     if caster:HasModifier("modifier_abyss_sword_rush_night_sword_qi_spell") then
         caster:AddNewModifier(caster, self, "modifier_abyss_sword_rush_night_sword_qi", {})
-        caster:AddNewModifier(caster, self, "modifier_tidehunter_anchor_smash_caster", {})
+        --胜率太低，暂时改回触发狂战特效 caster:AddNewModifier(caster, self, "modifier_tidehunter_anchor_smash_caster", {})
         caster:PerformAttack(target, true, true, true, true, false, false, true)
         caster:RemoveModifierByName("modifier_abyss_sword_rush_night_sword_qi")
-        caster:RemoveModifierByName("modifier_tidehunter_anchor_smash_caster")
+        --胜率太低，暂时改回触发狂战特效 caster:RemoveModifierByName("modifier_tidehunter_anchor_smash_caster")
     else
         if not target then return end
         if target:HasModifier("modifier_abyss_sword_rush_night_sword_qi_enemy") then return end
@@ -33,50 +36,49 @@ function abyss_sword_rush_night_sword_qi:OnProjectileHit(target,location)
             ability = self
         }
         ApplyDamage(dmgtable)
-        target:AddNewModifier(caster, self, "modifier_abyss_sword_rush_night_sword_qi_enemy", {duration = 0.5})
+        target:AddNewModifier(caster, self, "modifier_abyss_sword_rush_night_sword_qi_enemy", { duration = 0.5 })
     end
 end
-
 
 function abyss_sword_rush_night_sword_qi:OnSpellStart()
     local target = self:GetCursorPosition()
     local origin = self:GetCaster():GetAbsOrigin()
-    self:CreateProjectiles(origin,target,0,1)
+    self:CreateProjectiles(origin, target, 0, 1)
     EmitAnnouncerSoundForPlayer("npc_dota_hero_visage.vo.SwordQi.Cast", self:GetCaster():GetPlayerOwnerID())
-    self:GetCaster():AddNewModifier(caster, self, "modifier_abyss_sword_rush_night_sword_qi_spell", {duration = 0.5})
+    self:GetCaster():AddNewModifier(caster, self, "modifier_abyss_sword_rush_night_sword_qi_spell", { duration = 0.5 })
 end
 
-function abyss_sword_rush_night_sword_qi:CreateProjectiles(origin,target,angle,count)
+function abyss_sword_rush_night_sword_qi:CreateProjectiles(origin, target, angle, count)
     local caster = self:GetCaster()
     local width = self:GetSpecialValueFor("width")
     local length = self:GetSpecialValueFor("length")
     local speed = self:GetSpecialValueFor("speed")
     local vector = (target - origin):Normalized()
     local startangle = angle / 2
-    local step = angle/count
+    local step = angle / count
     if count == 1 then
         step = startangle
     end
     for i = 1, count, 1 do
         ProjectileManager:CreateLinearProjectile({
-            Ability				= self,
-            EffectName			= "particles/custom/abyss_sword/abyss_sword_rush_night_sword_qi_wave.vpcf",
-            vSpawnOrigin		= origin,
-            fDistance			= length,
-            fStartRadius		= width,
-            fEndRadius			= width,
-            Source				= caster,
-            bHasFrontalCone		= false,
-            bReplaceExisting	= false,
-            iUnitTargetTeam		= DOTA_UNIT_TARGET_TEAM_ENEMY,
-            iUnitTargetFlags	= DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
-            iUnitTargetType		= DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
-            fExpireTime 		= GameRules:GetGameTime() + 5.0,
-            bDeleteOnHit		= false,
-            vVelocity			= RotatePosition(Vector(0,0,0), QAngle(0,startangle - i*step, 0), vector) * speed,
-            bProvidesVision		= true,
-            iVisionRadius		= width,
-            iVisionTeamNumber	= caster:GetTeamNumber(),
+            Ability           = self,
+            EffectName        = "particles/custom/abyss_sword/abyss_sword_rush_night_sword_qi_wave.vpcf",
+            vSpawnOrigin      = origin,
+            fDistance         = length,
+            fStartRadius      = width,
+            fEndRadius        = width,
+            Source            = caster,
+            bHasFrontalCone   = false,
+            bReplaceExisting  = false,
+            iUnitTargetTeam   = DOTA_UNIT_TARGET_TEAM_ENEMY,
+            iUnitTargetFlags  = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+            iUnitTargetType   = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+            fExpireTime       = GameRules:GetGameTime() + 5.0,
+            bDeleteOnHit      = false,
+            vVelocity         = RotatePosition(Vector(0, 0, 0), QAngle(0, startangle - i * step, 0), vector) * speed,
+            bProvidesVision   = true,
+            iVisionRadius     = width,
+            iVisionTeamNumber = caster:GetTeamNumber(),
         })
     end
 
@@ -84,10 +86,13 @@ function abyss_sword_rush_night_sword_qi:CreateProjectiles(origin,target,angle,c
 end
 
 function modifier_abyss_sword_rush_night_sword_qi:RemoveOnDeath() return false end
+
 function modifier_abyss_sword_rush_night_sword_qi:IsHidden() return true end
+
 function modifier_abyss_sword_rush_night_sword_qi:IsPurgable() return false end
+
 function modifier_abyss_sword_rush_night_sword_qi:DeclareFunctions()
-    return {MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE}
+    return { MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE }
 end
 
 function modifier_abyss_sword_rush_night_sword_qi:GetModifierPreAttack_BonusDamage(params)
@@ -96,9 +101,13 @@ function modifier_abyss_sword_rush_night_sword_qi:GetModifierPreAttack_BonusDama
 end
 
 function modifier_abyss_sword_rush_night_sword_qi_spell:RemoveOnDeath() return true end
+
 function modifier_abyss_sword_rush_night_sword_qi_spell:IsHidden() return true end
+
 function modifier_abyss_sword_rush_night_sword_qi_spell:IsPurgable() return false end
 
 function modifier_abyss_sword_rush_night_sword_qi_enemy:RemoveOnDeath() return true end
+
 function modifier_abyss_sword_rush_night_sword_qi_enemy:IsHidden() return true end
+
 function modifier_abyss_sword_rush_night_sword_qi_enemy:IsPurgable() return false end
