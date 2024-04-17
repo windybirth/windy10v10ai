f30e60e7aac605b0a934d51c51935746119f80ec
76years-old-reliable-man <570648335@qq.com>
2024-04-17 23:08:25 +0800

    fix

---
+++ game/scripts/vscripts/modifiers/player/modifier_player_miao.lua
@@ -0,0 +1,54 @@
+modifier_player_miao = class({})
+
+function modifier_player_miao:IsPurgable() return false end
+function modifier_player_miao:RemoveOnDeath() return false end
+function modifier_player_miao:GetTexture() return "player/miao" end
+
+function modifier_player_miao:OnCreated()
+    self.icd = 50
+    self.icastrange = 500
+    self.ikangxing = 40
+    self.imovespeed = 200
+    self.ipctmanaregen = 4
+end
+
+
+function modifier_player_miao:DeclareFunctions()
+	return {
+        MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
+        MODIFIER_PROPERTY_CAST_RANGE_BONUS,
+        MODIFIER_PROPERTY_STATUS_RESISTANCE_STACKING,
+        MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT,
+        MODIFIER_PROPERTY_MANA_REGEN_TOTAL_PERCENTAGE,
+        MODIFIER_PROPERTY_IGNORE_MOVESPEED_LIMIT,
+		MODIFIER_PROPERTY_MOVESPEED_LIMIT,
+	}
+end
+
+function modifier_player_miao:GetModifierPercentageCooldown()
+    return self.icd
+end
+
+function modifier_player_miao:GetModifierCastRangeBonus()
+    return self.icastrange
+end
+
+function modifier_player_miao:GetModifierStatusResistanceStacking()
+    return self.ikangxing
+end
+
+function modifier_player_miao:GetModifierMoveSpeedBonus_Constant()
+    return self.imovespeed
+end
+
+function modifier_player_miao:GetModifierTotalPercentageManaRegen()
+    return self.ipctmanaregen
+end
+
+function modifier_player_miao:GetModifierMoveSpeed_Limit()
+    return 5000
+end
+
+function modifier_player_miao:GetModifierIgnoreMovespeedLimit()
+    return 1
+end
