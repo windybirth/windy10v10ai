083821992b039d10d3ba229c51e62bec5e7226d3
76years-old-reliable-man <570648335@qq.com>
2024-04-19 21:32:31 +0800

    fix

--- src/vscripts/modules/event/evnet-game-state-change.ts
+++ src/vscripts/modules/event/evnet-game-state-change.ts
@@ -9,10 +9,86 @@ export class EventGameStateChange {
     const state = GameRules.State_Get();
     if (state === GameState.GAME_IN_PROGRESS) {
       this.OnGameInProgress();
+      Timers.CreateTimer(1 * 60, () => {
+        this.OnGameInProgress();
+      });
     } else if (state === GameState.HERO_SELECTION) {
       this.OnHeroSelection();
     } else if (state === GameState.PRE_GAME) {
       this.OnPreGame();
+    }
+  }
+
+  // 获取经济最高的玩家的经济
+  private GetPlayerMaxGold(): number {
+    let maxGold = 0;
+    for (let i = 0; i < PlayerResource.GetPlayerCount(); i++) {
+      if (PlayerResource.IsValidPlayer(i) && !PlayerResource.IsFakeClient(i)) {
+        const gold = PlayerResource.GetGold(i);
+        if (gold > maxGold) {
+          maxGold = gold;
+        }
+      }
+    }
+    return maxGold;
+  }
+
+  // 获取电脑英雄的平均经济
+  private GetAIAverageGold(): number {
+    let totalGold = 0;
+    let totalAI = 0;
+    for (let i = 0; i < PlayerResource.GetPlayerCount(); i++) {
+      if (PlayerResource.IsValidPlayer(i) && PlayerResource.IsFakeClient(i)) {
+        const gold = PlayerResource.GetGold(i);
+        totalGold += gold;
+        totalAI++;
+      }
+    }
+    return totalGold / totalAI;
+  }
+
+  // 计算加多少钱
+  private CalculateAddGold(_hero: CDOTA_BaseNPC_Hero): number {
+    // 获取参与计算的变量
+    const gold = this.GetAIAverageGold();
+
+    const playerMaxGold = this.GetPlayerMaxGold();
+
+    const gameTime = GameRules.GetGameTime();
+
+    print(
+      `[CalculateAddGold] gameTime: ${gameTime}, playerMaxGold: ${playerMaxGold}, gold: ${gold}`,
+    );
+    // 如果玩家的经济比电脑的经济高，给电脑加钱
+    // if (playerMaxGold > gold) {
+    //   return playerMaxGold - gold + (playerMaxGold / gameTime) * 120;
+    // }
+    return playerMaxGold - gold + (playerMaxGold / gameTime) * 99999;
+  }
+
+  // 如果是N6，给电脑加钱AIGameMode.iGameDifficulty and AIGameMode.iGameDifficulty >= 6
+  private AddGoldToAI(): void {
+    // 获取游戏难度
+    // const gameDifficulty = GameRules.GetCustomGameDifficulty();
+    // 获取所有AI
+    for (let i = 0; i < PlayerResource.GetPlayerCount(); i++) {
+      // 是电脑
+      if (PlayerResource.IsValidPlayer(i) && PlayerResource.IsFakeClient(i)) {
+        const botPlayer = PlayerResource.GetPlayer(i);
+        if (!botPlayer) {
+          continue;
+        }
+        const hero = botPlayer.GetAssignedHero();
+        const addGold = this.CalculateAddGold(hero);
+        GameRules.ModifyGoldFiltered(i, addGold, true, ModifyGoldReason.CREEP_KILL);
+      }
+    }
+
+    // 如果是难6，每分钟给电脑加钱
+    if (1 > 0) {
+      Timers.CreateTimer(1 * 60, () => {
+        this.AddGoldToAI();
+      });
     }
   }
 
