a9f9e15fa80c59566e97d507ed1183f9738751f3
76years-old-reliable-man <570648335@qq.com>
2024-04-19 19:57:18 +0800

    fix

--- game/scripts/vscripts/modifiers/player/modifier_player_lumao.lua
+++ game/scripts/vscripts/modifiers/player/modifier_player_lumao.lua
@@ -1,5 +1,21 @@
 modifier_player_lumao = class({})
 
 function modifier_player_lumao:IsPurgable() return false end
+
 function modifier_player_lumao:RemoveOnDeath() return false end
+
 function modifier_player_lumao:GetTexture() return "player/lumao" end
+
+function modifier_player_lumao:OnCreated()
+    self.ikangxing = 30
+end
+
+function modifier_player_lumao:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_STATUS_RESISTANCE_STACKING,
+    }
+end
+
+function modifier_player_lumao:GetModifierStatusResistanceStacking()
+    return self.ikangxing
+end
