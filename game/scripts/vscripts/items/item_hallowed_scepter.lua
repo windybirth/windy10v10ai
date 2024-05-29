4fa59ed068ea7f52ff8c591cb69db1158fc3c8e5
windy <windybirth@gmail.com>
2024-05-29 21:56:55 +0900

    fix 7.36 lua API GetIntellect bug

--- game/scripts/vscripts/items/item_hallowed_scepter.lua
+++ game/scripts/vscripts/items/item_hallowed_scepter.lua
@@ -1,31 +1,38 @@
 if item_hallowed_scepter == nil then item_hallowed_scepter = class({}) end
 
-LinkLuaModifier("modifier_item_hallowed_scepter", 			"items/item_hallowed_scepter.lua", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_item_hallowed_scepter_active", 	"items/item_hallowed_scepter.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_hallowed_scepter", "items/item_hallowed_scepter.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_hallowed_scepter_active", "items/item_hallowed_scepter.lua", LUA_MODIFIER_MOTION_NONE)
 
 function item_hallowed_scepter:GetIntrinsicModifierName()
-	return "modifier_item_hallowed_scepter" end
+	return "modifier_item_hallowed_scepter"
+end
 
 function item_hallowed_scepter:OnSpellStart()
 	local caster = self:GetCaster()
 	local duration = self:GetSpecialValueFor("active_boost_duration")
 
-    EmitSoundOn("arcane_scepter", caster)
-    caster:AddNewModifier(caster, self, "modifier_item_hallowed_scepter_active", {duration = duration})
+	EmitSoundOn("arcane_scepter", caster)
+	caster:AddNewModifier(caster, self, "modifier_item_hallowed_scepter_active", { duration = duration })
 end
 
 if modifier_item_hallowed_scepter == nil then modifier_item_hallowed_scepter = class({}) end
 
-function modifier_item_hallowed_scepter:IsHidden()		return true end
-function modifier_item_hallowed_scepter:IsPurgable()	return false end
-function modifier_item_hallowed_scepter:RemoveOnDeath()	return false end
-function modifier_item_hallowed_scepter:GetAttributes()	return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_MULTIPLE + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE end
+function modifier_item_hallowed_scepter:IsHidden() return true end
+
+function modifier_item_hallowed_scepter:IsPurgable() return false end
+
+function modifier_item_hallowed_scepter:RemoveOnDeath() return false end
+
+function modifier_item_hallowed_scepter:GetAttributes()
+	return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_MULTIPLE +
+		MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
+end
 
 function modifier_item_hallowed_scepter:OnCreated()
 	self.stats_modifier_name = "modifier_item_hallowed_scepter_stats"
 	if IsServer() then
-        if not self:GetAbility() then self:Destroy() end
-    end
+		if not self:GetAbility() then self:Destroy() end
+	end
 	self.spell_amp_per_int = self:GetAbility():GetSpecialValueFor("spell_amp_per_int")
 
 	if not IsServer() then return end
@@ -51,21 +58,20 @@ function modifier_item_hallowed_scepter:DeclareFunctions()
 	}
 end
 
-
 function modifier_item_hallowed_scepter:GetModifierSpellAmplify_Percentage()
 	if self:GetAbility() and self:GetAbility():GetSecondaryCharges() == 1 then
-		local current_int = self:GetParent():GetIntellect()
+		local current_int = self:GetParent():GetIntellect(false)
 		return current_int * self.spell_amp_per_int
 	end
 end
 
-
-
 modifier_item_hallowed_scepter_active = class({})
 
-function modifier_item_hallowed_scepter_active:IsPermanent()	return false end
-function modifier_item_hallowed_scepter_active:IsHidden()		return false end
-function modifier_item_hallowed_scepter_active:IsPurgeable() 	return false end
+function modifier_item_hallowed_scepter_active:IsPermanent() return false end
+
+function modifier_item_hallowed_scepter_active:IsHidden() return false end
+
+function modifier_item_hallowed_scepter_active:IsPurgeable() return false end
 
 function modifier_item_hallowed_scepter_active:DeclareFunctions()
 	return {
@@ -78,7 +84,7 @@ function modifier_item_hallowed_scepter_active:GetModifierSpellAmplify_Percentag
 end
 
 function modifier_item_hallowed_scepter_active:GetTexture()
-    return "item_hallowed_scepter"
+	return "item_hallowed_scepter"
 end
 
 function modifier_item_hallowed_scepter_active:GetEffectName()
--- game/scripts/vscripts/items/item_magic_scepter.lua
+++ game/scripts/vscripts/items/item_magic_scepter.lua
@@ -1,31 +1,38 @@
 if item_magic_scepter == nil then item_magic_scepter = class({}) end
 
-LinkLuaModifier("modifier_item_magic_scepter", 			"items/item_magic_scepter.lua", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_item_magic_scepter_active", 	"items/item_magic_scepter.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_magic_scepter", "items/item_magic_scepter.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_magic_scepter_active", "items/item_magic_scepter.lua", LUA_MODIFIER_MOTION_NONE)
 
 function item_magic_scepter:GetIntrinsicModifierName()
-	return "modifier_item_magic_scepter" end
+	return "modifier_item_magic_scepter"
+end
 
 function item_magic_scepter:OnSpellStart()
 	local caster = self:GetCaster()
-	local duration = self:GetSpecialValueFor( "active_boost_duration" )
+	local duration = self:GetSpecialValueFor("active_boost_duration")
 
-    EmitSoundOn("arcane_scepter", caster)
-    caster:AddNewModifier(caster, self, "modifier_item_magic_scepter_active", {duration = duration})
+	EmitSoundOn("arcane_scepter", caster)
+	caster:AddNewModifier(caster, self, "modifier_item_magic_scepter_active", { duration = duration })
 end
 
 if modifier_item_magic_scepter == nil then modifier_item_magic_scepter = class({}) end
 
-function modifier_item_magic_scepter:IsHidden()			return true end
-function modifier_item_magic_scepter:IsPurgable()		return false end
-function modifier_item_magic_scepter:RemoveOnDeath()	return false end
-function modifier_item_magic_scepter:GetAttributes()	return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_MULTIPLE + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE end
+function modifier_item_magic_scepter:IsHidden() return true end
+
+function modifier_item_magic_scepter:IsPurgable() return false end
+
+function modifier_item_magic_scepter:RemoveOnDeath() return false end
+
+function modifier_item_magic_scepter:GetAttributes()
+	return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_MULTIPLE +
+		MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
+end
 
 function modifier_item_magic_scepter:OnCreated()
 	self.stats_modifier_name = "modifier_item_magic_scepter_stats"
 	if IsServer() then
-        if not self:GetAbility() then self:Destroy() end
-    end
+		if not self:GetAbility() then self:Destroy() end
+	end
 	self.spell_amp_per_int = self:GetAbility():GetSpecialValueFor("spell_amp_per_int")
 
 	if not IsServer() then return end
@@ -56,18 +63,18 @@ function modifier_item_magic_scepter:GetModifierSpellAmplify_Percentage()
 		return 0
 	end
 	if self:GetAbility() and self:GetAbility():GetSecondaryCharges() == 1 then
-		local current_int = self:GetParent():GetIntellect()
+		local current_int = self:GetParent():GetIntellect(false)
 		return current_int * self.spell_amp_per_int
 	end
 end
 
-
-
 modifier_item_magic_scepter_active = class({})
 
-function modifier_item_magic_scepter_active:IsPermanent()	return false end
-function modifier_item_magic_scepter_active:IsHidden()		return false end
-function modifier_item_magic_scepter_active:IsPurgeable() 	return false end
+function modifier_item_magic_scepter_active:IsPermanent() return false end
+
+function modifier_item_magic_scepter_active:IsHidden() return false end
+
+function modifier_item_magic_scepter_active:IsPurgeable() return false end
 
 function modifier_item_magic_scepter_active:DeclareFunctions()
 	return {
@@ -75,18 +82,18 @@ function modifier_item_magic_scepter_active:DeclareFunctions()
 	}
 end
 
-
 function modifier_item_magic_scepter_active:GetModifierSpellAmplify_Percentage()
 	return self:GetAbility():GetSpecialValueFor("active_boost")
 end
 
-
 function modifier_item_magic_scepter_active:GetTexture()
-    return "item_magic_scepter"
+	return "item_magic_scepter"
 end
 
 function modifier_item_magic_scepter_active:GetEffectName()
-	return "particles/items4_fx/arcane_scepter.vpcf" end
+	return "particles/items4_fx/arcane_scepter.vpcf"
+end
 
 function modifier_item_magic_scepter_active:GetEffectAttachType()
-	return PATTACH_ABSORIGIN_FOLLOW end
+	return PATTACH_ABSORIGIN_FOLLOW
+end
