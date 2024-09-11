a858552a37823c23e439400acc7b7a23cbf065c7
windy <windybirth@gmail.com>
2024-09-11 21:55:40 +0900

    Update bot_think_item_build.lua

--- game/scripts/vscripts/bot/bot_think_item_build.lua
+++ game/scripts/vscripts/bot/bot_think_item_build.lua
@@ -51,7 +51,8 @@ local function addTome(k, v)
   for i = 1, amount do
     table.insert(v, "item_tome_of_strength")
     table.insert(v, "item_tome_of_agility")
-    if i % 2 == 0 then
+    -- 最大吃N本智力书，防止魔抗溢出
+    if i < 4 then
       table.insert(v, "item_tome_of_intelligence")
     end
   end
