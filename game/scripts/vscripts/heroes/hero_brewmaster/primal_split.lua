95ba02787045f460051c719e57637ef250f0b0eb
windy <windybirth@gmail.com>
2024-09-23 21:55:43 +0900

    remove active_multiplier

--- game/scripts/vscripts/heroes/hero_brewmaster/primal_split.lua
+++ game/scripts/vscripts/heroes/hero_brewmaster/primal_split.lua
@@ -1,7 +1,8 @@
 --------------------------------------------------------------------------------
 brewmaster_primal_split_lua = class({})
 --------------------------------------------------------------------------------
-LinkLuaModifier( "modifier_brewmaster_primal_split_lua_duration", "heroes/hero_brewmaster/primal_split", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_brewmaster_primal_split_lua_duration", "heroes/hero_brewmaster/primal_split",
+	LUA_MODIFIER_MOTION_NONE)
 --------------------------------------------------------------------------------
 -- Ability Start
 function brewmaster_primal_split_lua:OnSpellStart()
@@ -11,33 +12,33 @@ function brewmaster_primal_split_lua:OnSpellStart()
 	local duration = self:GetSpecialValueFor("duration")
 	-- add modifier
 	caster:AddNewModifier(
-		caster, -- player source
-		self, -- ability source
+		caster,                                    -- player source
+		self,                                      -- ability source
 		"modifier_brewmaster_primal_split_lua_duration", -- modifier name
-		{ duration = duration } -- kv
+		{ duration = duration }                    -- kv
 	)
 	-- effects
 	local sound_cast = "Hero_Brewmaster.PrimalSplit.Spawn"
-	EmitSoundOn( sound_cast, caster )
+	EmitSoundOn(sound_cast, caster)
 end
-
-
-
 
 --------------------------------------------------------------------------------
 modifier_brewmaster_primal_split_lua_duration = class({})
 --------------------------------------------------------------------------------
 -- Classifications
 function modifier_brewmaster_primal_split_lua_duration:IsHidden() return false end
+
 function modifier_brewmaster_primal_split_lua_duration:IsDebuff() return false end
+
 function modifier_brewmaster_primal_split_lua_duration:IsStunDebuff() return false end
+
 function modifier_brewmaster_primal_split_lua_duration:IsPurgable() return false end
+
 --------------------------------------------------------------------------------
 -- Initializations
-function modifier_brewmaster_primal_split_lua_duration:OnCreated( kv )
+function modifier_brewmaster_primal_split_lua_duration:OnCreated(kv)
 	self.dodge_chance = self:GetAbility():GetSpecialValueFor("dodge_chance")
 	-- self.crit_chance = self:GetAbility():GetSpecialValueFor("crit_chance")
-	self.active_multiplier = self:GetAbility():GetSpecialValueFor("active_multiplier")
 	-- self.crit_multiplier = self:GetAbility():GetSpecialValueFor("crit_multiplier")
 	self.attack_speed = self:GetAbility():GetSpecialValueFor("attack_speed")
 	self.magic_resist = self:GetAbility():GetSpecialValueFor("magic_resist")
@@ -67,7 +68,7 @@ function modifier_brewmaster_primal_split_lua_duration:OnCreated( kv )
 	self.bonus_status_resist = self.ability:GetSpecialValueFor("bonus_status_resist")
 end
 
-function modifier_brewmaster_primal_split_lua_duration:OnRefresh( kv ) end
+function modifier_brewmaster_primal_split_lua_duration:OnRefresh(kv) end
 
 function modifier_brewmaster_primal_split_lua_duration:OnRemoved() end
 
@@ -76,12 +77,13 @@ function modifier_brewmaster_primal_split_lua_duration:OnDestroy()
 	if IsValidEntity(self.parent) then
 		-- play effects
 		local sound_cast = "Hero_Brewmaster.PrimalSplit.Return"
-		EmitSoundOn( sound_cast, self.parent )
+		EmitSoundOn(sound_cast, self.parent)
 		-- ultimate hangover
 		if self.parent:HasAbility("brewmaster_belligerent") then
 			local bellAB = self.parent:FindAbilityByName("brewmaster_belligerent")
 			if bellAB then
-				self.parent:AddNewModifier(self.parent, bellAB, "modifier_brewmaster_belligerent_damage", { duration = bellAB:GetSpecialValueFor("damage_duration_split")})
+				self.parent:AddNewModifier(self.parent, bellAB, "modifier_brewmaster_belligerent_damage",
+					{ duration = bellAB:GetSpecialValueFor("damage_duration_split") })
 			end
 		end
 	end
@@ -101,24 +103,28 @@ function modifier_brewmaster_primal_split_lua_duration:DeclareFunctions()
 		MODIFIER_PROPERTY_PREATTACK_CRITICALSTRIKE,
 
 		MODIFIER_PROPERTY_STATUS_RESISTANCE_STACKING,
-  		MODIFIER_EVENT_ON_ATTACK_LANDED,
+		MODIFIER_EVENT_ON_ATTACK_LANDED,
 	}
 	return funcs
 end
 
-function modifier_brewmaster_primal_split_lua_duration:GetModifierPhysicalArmorBonus() return self.armor * self.active_multiplier end
-function modifier_brewmaster_primal_split_lua_duration:GetModifierMagicalResistanceBonus() return self.magic_resist * self.active_multiplier end
+function modifier_brewmaster_primal_split_lua_duration:GetModifierPhysicalArmorBonus() return self.armor end
 
-function modifier_brewmaster_primal_split_lua_duration:GetModifierEvasion_Constant() return self.dodge_chance * self.active_multiplier end
-function modifier_brewmaster_primal_split_lua_duration:GetModifierMoveSpeedBonus_Percentage() return self.bonus_move_speed * self.active_multiplier end
+function modifier_brewmaster_primal_split_lua_duration:GetModifierMagicalResistanceBonus() return self.magic_resist end
 
-function modifier_brewmaster_primal_split_lua_duration:GetModifierAttackSpeedBonus_Constant() return self.attack_speed * self.active_multiplier end
+function modifier_brewmaster_primal_split_lua_duration:GetModifierEvasion_Constant() return self.dodge_chance end
+
+function modifier_brewmaster_primal_split_lua_duration:GetModifierMoveSpeedBonus_Percentage() return self
+	.bonus_move_speed end
+
+function modifier_brewmaster_primal_split_lua_duration:GetModifierAttackSpeedBonus_Constant() return self.attack_speed end
+
 function modifier_brewmaster_primal_split_lua_duration:GetModifierPreAttack_CriticalStrike(keys)
 	if not IsServer() or self:GetParent():IsIllusion() then return end
-    if keys.attacker == self.parent and not keys.target:IsBuilding() and not keys.target:IsOther() then
-        if RollPseudoRandomPercentage( self.crit_chance * self.active_multiplier, DOTA_PSEUDO_RANDOM_BREWMASTER_CRIT, self.parent) then
+	if keys.attacker == self.parent and not keys.target:IsBuilding() and not keys.target:IsOther() then
+		if RollPseudoRandomPercentage(self.crit_chance, DOTA_PSEUDO_RANDOM_BREWMASTER_CRIT, self.parent) then
 			self.parent:EmitSound("Hero_Brewmaster.Brawler.Crit")
-			return self.crit_multiplier * self.active_multiplier
+			return self.crit_multiplier
 		else
 			return 0
 		end
@@ -128,11 +134,12 @@ end
 function modifier_brewmaster_primal_split_lua_duration:OnAttackLanded(keys)
 	if not IsServer() then return end
 	if keys.attacker ~= self.parent or keys.target:IsBuilding() or not keys.target:IsAlive() then return end
-	keys.target:AddNewModifier(self.parent, self.ability, "modifier_brewmaster_void_brawler_slow", { duration = self.slow_duration })
+	keys.target:AddNewModifier(self.parent, self.ability, "modifier_brewmaster_void_brawler_slow",
+		{ duration = self.slow_duration })
 end
 
 function modifier_brewmaster_primal_split_lua_duration:GetModifierStatusResistanceStacking()
-	return self.bonus_status_resist * self.active_multiplier
+	return self.bonus_status_resist
 end
 
 --------------------------------------------------------------------------------
