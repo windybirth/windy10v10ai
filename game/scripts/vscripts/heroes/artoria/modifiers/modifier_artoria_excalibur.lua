f0b3d43deecc59071fcc12125d2be3c37a0b4e93
windy <windybirth@gmail.com>
2024-11-03 14:53:13 +0900

    enable

--- game/scripts/vscripts/heroes/artoria/modifiers/modifier_artoria_excalibur.lua
+++ game/scripts/vscripts/heroes/artoria/modifiers/modifier_artoria_excalibur.lua
@@ -1,9 +1,13 @@
-modifier_saber_excalibur = class ({})
+modifier_saber_excalibur = class({})
 
 function modifier_saber_excalibur:IsHidden() return true end
+
 function modifier_saber_excalibur:IsDebuff() return false end
+
 function modifier_saber_excalibur:IsPurgable() return false end
+
 function modifier_saber_excalibur:IsPurgeException() return false end
+
 function modifier_saber_excalibur:RemoveOnDeath() return false end
 
 function modifier_saber_excalibur:OnCreated()
@@ -11,6 +15,7 @@ function modifier_saber_excalibur:OnCreated()
         self:StartIntervalThink(FrameTime())
     end
 end
+
 function modifier_saber_excalibur:OnRefresh()
     if IsServer() then
 
@@ -21,7 +26,12 @@ function modifier_saber_excalibur:OnIntervalThink()
     if IsServer() then
         local artoria_ultimate_excalibur = self:GetParent():FindAbilityByName("artoria_ultimate_excalibur")
         if artoria_ultimate_excalibur and not artoria_ultimate_excalibur:IsNull() then
-            if self:GetParent():HasItemInInventory("item_excalibur") then
+            local has_excalibur = self:GetParent():HasItemInInventory("item_excalibur") or
+                self:GetParent():HasItemInInventory("item_rapier_ultra") or
+                self:GetParent():HasItemInInventory("item_rapier_ultra_bot") or
+                self:GetParent():HasItemInInventory("item_rapier_ultra_bot_1")
+
+            if has_excalibur then
                 if artoria_ultimate_excalibur:IsHidden() then
                     artoria_ultimate_excalibur:SetHidden(false)
                 end
