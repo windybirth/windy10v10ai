7e42004f09364f5521bc09d0e5895b41c8219001
windy <windybirth@gmail.com>
2024-11-03 10:40:00 +0900

    format

--- game/scripts/vscripts/heroes/artoria/modifiers/modifier_artoria_instinct.lua
+++ game/scripts/vscripts/heroes/artoria/modifiers/modifier_artoria_instinct.lua
@@ -36,27 +36,27 @@ function modifier_artoria_instinct:GetModifierStatusResistanceStacking()
 	return self.status_res
 end
 
-function modifier_artoria_instinct:GetAbsorbSpell( params )
+function modifier_artoria_instinct:GetAbsorbSpell(params)
 	if IsServer() then
 		-- if caster is teammate, do nothing
 		if params.ability:GetCaster():GetTeamNumber() == self:GetParent():GetTeamNumber() then return end
 		if self:GetAbility():IsFullyCastable() then
 			-- use resources
-			self:GetAbility():UseResources( false, false, false, true )
+			self:GetAbility():UseResources(false, false, false, true)
 
-			self:PlayEffects( true )
+			self:PlayEffects(true)
 			return 1
 		end
 	end
 end
 
 modifier_artoria_instinct.reflected_spell = nil
-function modifier_artoria_instinct:GetReflectSpell( params )
+function modifier_artoria_instinct:GetReflectSpell(params)
 	if IsServer() then
 		-- if caster is teammate, do nothing
 		if params.ability:GetCaster():GetTeamNumber() == self:GetParent():GetTeamNumber() then return end
 		-- If unable to reflect due to the source ability
-		if params.ability==nil or self.reflect_exceptions[params.ability:GetAbilityName()] then
+		if params.ability == nil or self.reflect_exceptions[params.ability:GetAbilityName()] then
 			return 0
 		end
 
@@ -65,32 +65,33 @@ function modifier_artoria_instinct:GetReflectSpell( params )
 			self.reflect = true
 
 			-- remove previous ability
-			if self.reflected_spell~=nil then
-				self:GetParent():RemoveAbility( self.reflected_spell:GetAbilityName() )
+			if self.reflected_spell ~= nil then
+				self:GetParent():RemoveAbility(self.reflected_spell:GetAbilityName())
 			end
 
 			-- copy the ability
 			local sourceAbility = params.ability
-			local selfAbility = self:GetParent():AddAbility( sourceAbility:GetAbilityName() )
-			selfAbility:SetLevel( sourceAbility:GetLevel() )
-			selfAbility:SetStolen( true )
-			selfAbility:SetHidden( true )
+			local selfAbility = self:GetParent():AddAbility(sourceAbility:GetAbilityName())
+			selfAbility:SetLevel(sourceAbility:GetLevel())
+			selfAbility:SetStolen(true)
+			selfAbility:SetHidden(true)
 
 			-- store the ability
 			self.reflected_spell = selfAbility
 
 			-- cast the ability
-			self:GetParent():SetCursorCastTarget( sourceAbility:GetCaster() )
+			self:GetParent():SetCursorCastTarget(sourceAbility:GetCaster())
 			selfAbility:CastAbility()
 
 			-- play effects
-			self:PlayEffects( false )
+			self:PlayEffects(false)
 			return 1
 		end
 	end
 end
+
 --------------------------------------------------------------------------------
-function modifier_artoria_instinct:PlayEffects( bBlock )
+function modifier_artoria_instinct:PlayEffects(bBlock)
 	-- Get Resources
 	local particle_cast = ""
 	local sound_cast = ""
@@ -102,7 +103,7 @@ function modifier_artoria_instinct:PlayEffects( bBlock )
 	end
 
 	-- Play particles
-	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_ABSORIGIN_FOLLOW, self:GetParent() )
+	local effect_cast = ParticleManager:CreateParticle(particle_cast, PATTACH_ABSORIGIN_FOLLOW, self:GetParent())
 	ParticleManager:SetParticleControlEnt(
 		effect_cast,
 		0,
@@ -110,12 +111,12 @@ function modifier_artoria_instinct:PlayEffects( bBlock )
 		PATTACH_POINT_FOLLOW,
 		"attach_hitloc",
 		self:GetParent():GetOrigin(), -- unknown
-		true -- unknown, true
+		true                    -- unknown, true
 	)
-	ParticleManager:ReleaseParticleIndex( effect_cast )
+	ParticleManager:ReleaseParticleIndex(effect_cast)
 
 	-- Play sounds
-	EmitSoundOn( "artoria_instinct", self:GetParent() )
+	EmitSoundOn("artoria_instinct", self:GetParent())
 end
 
 modifier_artoria_instinct.reflect_exceptions = {
