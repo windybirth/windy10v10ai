2b7d9552784c66550ffef41a357ad9f804e240da
Nemesis041 <tibbetts041@gmail.com>
2024-11-08 04:41:11 -0800

    Kez update (#1074)
    * Enabled Kez
    
    * New Hero's Abilities Added and Rescaled
    
    -Granted all of Kez' abilities one extra level, and rebalanced scaling properties to account for the increased amount of levels.
    --Some aspects of some of his abilities are stronger than in the base game. Keep an eye on how he performs in games and adjust accordingly.
    
    * Oversight Correction
    
    -Kez' abilities are allowed to be leveled up to 5/4 for basic/ultimates now.
    
    * Fix Kez abilities
    
    ---------
    
    Co-authored-by: windy <windybirth@gmail.com>

--- src/vscripts/api/game.ts
+++ src/vscripts/api/game.ts
@@ -32,7 +32,7 @@ class EndGameInfo {
 }
 
 export class Game {
-  private static VERSION = "v3.39";
+  private static VERSION = "v3.40";
   constructor() {}
 
   public SendEndGameInfo(endData: EndGameInfo) {
